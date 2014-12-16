1f4e8d390665ee33ebc8b0a114467b71f2b92bd6
Joshua Arnott <josh@snorfalorpagus.net>
2014-12-16 18:08:39 +0000

    Added more tests for XML reader.

--- test_xml.py
+++ test_xml.py
@@ -3,11 +3,37 @@
 
 from __future__ import print_function
 
+import pywr
 import xmlutils
-import matplotlib.pyplot as pyplot
 
-data = file('simple1.xml', 'r').read()
-model = xmlutils.parse_xml(data)
+def test_simple1():
+	'''Test parsing a simple XML document'''
+	# parse the XML into a model
+	data = file('simple1.xml', 'r').read()
+	model = xmlutils.parse_xml(data)
 
-model.plot()
-pyplot.show()
+	# metadata
+	assert(model.metadata['title'] == 'Simple 1')
+	assert(model.metadata['description'] == 'A very simple example.')
+
+	# node names
+	nodes = model.nodes()
+	assert(len(nodes) == 3)
+	name_to_node = {}
+	for node in nodes:
+		name_to_node[node.name] = node
+	supply1 = name_to_node['supply1']
+	link1 = name_to_node['link1']
+	demand1 = name_to_node['demand1']
+
+	# node types
+	assert(type(supply1) is pywr.Supply)
+	assert(type(link1) is pywr.Link)
+	assert(type(demand1) is pywr.Demand)
+
+	# node positions
+	assert(supply1.position == (1,1))
+	assert(link1.position == (2,1))
+	assert(demand1.position == (3,1))
+
+	model.check()
