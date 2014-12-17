7a27fdb4794c74ae26a31e85cc84727c0caf1bd3
Joshua Arnott <josh@snorfalorpagus.net>
2014-12-17 23:33:55 +0000

    Added first of many tests of model results.

---
+++ tests/test_run.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import os
+import datetime
+
+import pywr.core
+import pywr.xmlutils
+
+def test_run_simple1():
+    # parse the XML into a model
+    data = file(os.path.join(os.path.dirname(__file__), 'simple1.xml'), 'r').read()
+    model = pywr.xmlutils.parse_xml(data)
+
+    # run the model
+    t0 = model.timestamp
+    result = model.step()
+    
+    # check results
+    assert(result == ('optimal', 10.0, 10.0))
+    
+    # check the timestamp incremented
+    assert(model.timestamp - t0 == datetime.timedelta(1))
+        
