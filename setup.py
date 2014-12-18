91a9b9db145d9aa1f1cf7d0c38fedc96920560b0
Joshua Arnott <josh@snorfalorpagus.net>
2014-12-18 18:23:11 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pywr',
+    version='0.1',
+    description='Python Water Resource model',
+    author='Joshua Arnott',
+    author_email='josh@snorfalorpagus.net',
+    url='http://snorf.net/pywr/',
+    packages=['pywr'],
+)
