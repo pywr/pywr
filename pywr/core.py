pywr/pywr

commit 14be5e8c8cd26fc1f079502ab606e52b979d72a6

Ported existing code to Python 3.
Requires PyGLPK v0.4 for Python 3.

pywr/core.py
@@ -134,7 +134,7 @@ class SolverMeta(type):
         cls.solvers[newclass.name.lower()] = newclass
         return newclass
 
-class Solver(object):
+class Solver(object, metaclass=SolverMeta):
     '''Solver base class from which all solvers should inherit'''
     __metaclass__ = SolverMeta
     name = 'default'
@@ -180,7 +180,7 @@ class NodeMeta(type):
         super(NodeMeta, cls).__init__(name, bases, dct)
         node_registry[name.lower()] = cls
 
-class Node(object):
+class Node(object, metaclass=NodeMeta):
     '''Base object from which all other nodes inherit'''
     __metaclass__ = NodeMeta
     
@@ -341,4 +341,4 @@ class Reservoir(Supply, Demand):
         # check volume doesn't exceed maximum volume
         assert(self.properties['max_volume'].value(index) >= self.properties['current_volume'].value(index))
 
-import solvers
+from . import solvers

pywr/licenses.py
@@ -14,7 +14,7 @@ class License(object):
         if cls is License:
             raise TypeError('License cannot be instantiated directly')
         else:
-            return object.__new__(cls, *args, **kwargs)
+            return object.__new__(cls)
     def available(self, index):
         raise NotImplementedError()
     def resource_state(self, index):

pywr/solvers/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
 try:
-    from pywr_glpk import SolverGLPK
+    from .pywr_glpk import SolverGLPK
 except:
     pass

tests/test_run.py
@@ -14,7 +14,8 @@ import pywr.licenses
 def test_run_simple1():
     '''Test the most basic model possible'''
     # parse the XML into a model
-    data = file(os.path.join(os.path.dirname(__file__), 'simple1.xml'), 'r').read()
+    with open(os.path.join(os.path.dirname(__file__), 'simple1.xml'), 'r') as f:
+        data = f.read()
     model = pywr.xmlutils.parse_xml(data)
 
     # run the model
@@ -32,7 +33,8 @@ def test_run_reservoir1():
     
     Without an additional supply the reservoir should empty and cause a failure.
     '''
-    data = file(os.path.join(os.path.dirname(__file__), 'reservoir1.xml'), 'r').read()
+    with open(os.path.join(os.path.dirname(__file__), 'reservoir1.xml'), 'r') as f:
+        data = f.read()
     model = pywr.xmlutils.parse_xml(data)
     model.check()
 
@@ -46,7 +48,8 @@ def test_run_reservoir2():
     The river abstraction should refill the reservoir, but not quickly enough
     to keep up with the demand.
     '''
-    data = file(os.path.join(os.path.dirname(__file__), 'reservoir2.xml'), 'r').read()
+    with open(os.path.join(os.path.dirname(__file__), 'reservoir2.xml'), 'r') as f:
+        data = f.read()
     model = pywr.xmlutils.parse_xml(data)
     model.check()
     
@@ -56,7 +59,8 @@ def test_run_reservoir2():
 
 def test_run_river1():
     '''Test a river abstraction with a simple catchment'''
-    data = file(os.path.join(os.path.dirname(__file__), 'river1.xml'), 'r').read()
+    with open(os.path.join(os.path.dirname(__file__), 'river1.xml'), 'r') as f:
+        data = f.read()
     model = pywr.xmlutils.parse_xml(data)
     model.check()
     
@@ -65,7 +69,8 @@ def test_run_river1():
 
 def test_run_river2():
     '''Test a river abstraction with two catchments, a confluence and a split'''
-    data = file(os.path.join(os.path.dirname(__file__), 'river2.xml'), 'r').read()
+    with open(os.path.join(os.path.dirname(__file__), 'river2.xml'), 'r') as f:
+        data = f.read()
     model = pywr.xmlutils.parse_xml(data)
     model.check()
     
@@ -73,7 +78,8 @@ def test_run_river2():
     assert(result[0:3] == ('optimal', 12.0, 9.25))
 
 def test_run_timeseries1():
-    data = file(os.path.join(os.path.dirname(__file__), 'timeseries1.xml'), 'r').read()
+    with open(os.path.join(os.path.dirname(__file__), 'timeseries1.xml'), 'r') as f:
+        data = f.read()
     model = pywr.xmlutils.parse_xml(data)
     model.check()
     
@@ -92,7 +98,8 @@ def test_run_timeseries1():
     assert(supplied == [23.0, 22.14, 22.57, 23.0, 23.0])
 
 def test_run_cost1():
-    data = file(os.path.join(os.path.dirname(__file__), 'cost1.xml'), 'r').read()
+    with open(os.path.join(os.path.dirname(__file__), 'cost1.xml'), 'r') as f:
+        data = f.read()
     model = pywr.xmlutils.parse_xml(data)
     model.check()
     
@@ -103,7 +110,7 @@ def test_run_cost1():
     result = model.step()
     # check entire demand was supplied by supply1
     assert(result[0:3] == ('optimal', 10.0, 10.0))
-    assert(result[3].items() == [((nodes['supply1'], nodes['demand1']), 10.0)])
+    assert(list(result[3].items()) == [((nodes['supply1'], nodes['demand1']), 10.0)])
     
     # increase demand to more than supply1 can provide on it's own
     # and check that supply2 is used to pick up the slack
@@ -119,7 +126,8 @@ def test_run_cost1():
     assert(result[0:3] == ('optimal', 40.0, 30.0))
 
 def test_run_license():
-    data = file(os.path.join(os.path.dirname(__file__), 'simple1.xml'), 'r').read()
+    with open(os.path.join(os.path.dirname(__file__), 'simple1.xml'), 'r') as f:
+        data = f.read()
     model = pywr.xmlutils.parse_xml(data)
     model.timestamp = datetime.datetime(2015, 1, 1)
     
@@ -151,7 +159,8 @@ def test_run_license():
 
 def test_run_bottleneck():
     '''Test max flow constraint on intermediate nodes is upheld'''
-    data = file(os.path.join(os.path.dirname(__file__), 'bottleneck.xml'), 'r').read()
+    with open(os.path.join(os.path.dirname(__file__), 'bottleneck.xml'), 'r') as f:
+        data = f.read()
     model = pywr.xmlutils.parse_xml(data)
     model.check()
     result = model.step()

tests/test_xml.py
@@ -9,33 +9,34 @@ import pywr.core
 import pywr.xmlutils
 
 def test_simple1():
-	'''Test parsing a simple XML document'''
-	# parse the XML into a model
-	data = file(os.path.join(os.path.dirname(__file__), 'simple1.xml'), 'r').read()
-	model = pywr.xmlutils.parse_xml(data)
+    '''Test parsing a simple XML document'''
+    # parse the XML into a model
+    with open(os.path.join(os.path.dirname(__file__), 'simple1.xml'), 'r') as f:
+        data = f.read()
+    model = pywr.xmlutils.parse_xml(data)
 
-	# metadata
-	assert(model.metadata['title'] == 'Simple 1')
-	assert(model.metadata['description'] == 'A very simple example.')
+    # metadata
+    assert(model.metadata['title'] == 'Simple 1')
+    assert(model.metadata['description'] == 'A very simple example.')
 
-	# node names
-	nodes = model.nodes()
-	assert(len(nodes) == 3)
-	name_to_node = {}
-	for node in nodes:
-		name_to_node[node.name] = node
-	supply1 = name_to_node['supply1']
-	link1 = name_to_node['link1']
-	demand1 = name_to_node['demand1']
+    # node names
+    nodes = model.nodes()
+    assert(len(nodes) == 3)
+    name_to_node = {}
+    for node in nodes:
+        name_to_node[node.name] = node
+    supply1 = name_to_node['supply1']
+    link1 = name_to_node['link1']
+    demand1 = name_to_node['demand1']
 
-	# node types
-	assert(type(supply1) is pywr.core.Supply)
-	assert(type(link1) is pywr.core.Link)
-	assert(type(demand1) is pywr.core.Demand)
+    # node types
+    assert(type(supply1) is pywr.core.Supply)
+    assert(type(link1) is pywr.core.Link)
+    assert(type(demand1) is pywr.core.Demand)
 
-	# node positions
-	assert(supply1.position == (1,1))
-	assert(link1.position == (2,1))
-	assert(demand1.position == (3,1))
+    # node positions
+    assert(supply1.position == (1,1))
+    assert(link1.position == (2,1))
+    assert(demand1.position == (3,1))
 
-	model.check()
+    model.check()

commit e2518f98fb5dfc1182dd452675e7ad2efc25a31e

Fixed regression in 14be5e8 related to metaclasses.
`six` module now use for Python 2 + 3 compatibility.

pywr/core.py
@@ -11,6 +11,7 @@ import numpy as np
 import inspect
 import pandas
 import datetime
+from six import with_metaclass
 
 import warnings
 warnings.simplefilter(action = "ignore", category = FutureWarning)
@@ -134,9 +135,8 @@ class SolverMeta(type):
         cls.solvers[newclass.name.lower()] = newclass
         return newclass
 
-class Solver(object, metaclass=SolverMeta):
+class Solver(with_metaclass(SolverMeta)):
     '''Solver base class from which all solvers should inherit'''
-    __metaclass__ = SolverMeta
     name = 'default'
     def solve(self, model):
         raise NotImplementedError('Solver should be subclassed to provide solve()')
@@ -180,9 +180,8 @@ class NodeMeta(type):
         super(NodeMeta, cls).__init__(name, bases, dct)
         node_registry[name.lower()] = cls
 
-class Node(object, metaclass=NodeMeta):
+class Node(with_metaclass(NodeMeta)):
     '''Base object from which all other nodes inherit'''
-    __metaclass__ = NodeMeta
     
     def __init__(self, model, position=None, name=None, **kwargs):
         self.model = model