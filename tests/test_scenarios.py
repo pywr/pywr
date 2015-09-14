91a0f2940904d01e82cd11aa3f69ba5b9d12e0e4
James Tomlinson <tomo.bbe@gmail.com>
2015-09-14 20:42:14 +0100

    Fixed typo in test_scenario.py

--- tests/test_scenarios.py
+++ tests/test_scenarios.py
@@ -6,7 +6,7 @@ A series of tests of the Scenario objects and associated infrastructure
 """
 import pywr.core
 from helpers import assert_model
-# To gete simple_linear_model fixture
+# To get simple_linear_model fixture
 from test_analytical import simple_linear_model
 from numpy.testing import assert_equal
 
