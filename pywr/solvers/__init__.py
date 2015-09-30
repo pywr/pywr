33f0ad9abda2869f5abd7e2fb8632a7f06568c9b
James Tomlinson <tomo.bbe@gmail.com>
2015-09-30 20:06:06 +0100

    Reworked Solver subclasses in to a try-except-else clauses so they are only created if their cython modules are correctly loaded.

--- pywr/solvers/__init__.py
+++ pywr/solvers/__init__.py
@@ -5,42 +5,43 @@ try:
     from .cython_glpk import CythonGLPKSolver as cy_CythonGLPKSolver
 except ImportError:
     pass
+else:
+    class CythonGLPKSolver(Solver):
+        """Python wrapper of Cython GLPK solver.
+
+        This is required to subclass Solver and get the metaclass magic.
+        """
+        name = 'GLPK'
+
+        def __init__(self, *args, **kwargs):
+            super(CythonGLPKSolver, self).__init__(*args, **kwargs)
+            self._cy_solver = cy_CythonGLPKSolver()
+
+        def setup(self, model):
+            return self._cy_solver.setup(model)
+
+        def solve(self, model):
+            return self._cy_solver.solve(model)
+
 
 try:
     from .cython_lpsolve import CythonLPSolveSolver as cy_CythonLPSolveSolver
 except ImportError:
     pass
+else:
+    class CythonLPSolveSolver(Solver):
+        """Python wrapper of Cython LPSolve55 solver.
 
+        This is required to subclass Solver and get the metaclass magic.
+        """
+        name = 'lpsolve'
 
-class CythonGLPKSolver(Solver):
-    """Python wrapper of Cython GLPK solver.
+        def __init__(self, *args, **kwargs):
+            super(CythonLPSolveSolver, self).__init__(*args, **kwargs)
+            self._cy_solver = cy_CythonLPSolveSolver()
 
-    This is required to subclass Solver and get the metaclass magic.
-    """
-    name = 'GLPK'
-    def __init__(self, *args, **kwargs):
-        super(CythonGLPKSolver, self).__init__(*args, **kwargs)
-        self._cy_solver = cy_CythonGLPKSolver()
+        def setup(self, model):
+            return self._cy_solver.setup(model)
 
-    def setup(self, model):
-        return self._cy_solver.setup(model)
-
-    def solve(self, model):
-        return self._cy_solver.solve(model)
-
-
-class CythonLPSolveSolver(Solver):
-    """Python wrapper of Cython LPSolve55 solver.
-
-    This is required to subclass Solver and get the metaclass magic.
-    """
-    name = 'lpsolve'
-    def __init__(self, *args, **kwargs):
-        super(CythonLPSolveSolver, self).__init__(*args, **kwargs)
-        self._cy_solver = cy_CythonLPSolveSolver()
-
-    def setup(self, model):
-        return self._cy_solver.setup(model)        
-
-    def solve(self, model):
-        return self._cy_solver.solve(model)
+        def solve(self, model):
+            return self._cy_solver.solve(model)
