53a8ab19a3f7f4fbc7cc07f0471d28489c681b64
James Tomlinson <tomo.bbe@gmail.com>
2015-10-09 10:43:04 +0100

    Removed the need for an Output (or Demand) column in the linear programme.
    This has only been applied to the GLPK solver, and passes the existing tests. It reduces the number of rows & columns in the LP which is hopefully an efficiency.

--- pywr/solvers/cython_glpk.pyx
+++ pywr/solvers/cython_glpk.pyx
@@ -10,15 +10,12 @@ cdef class CythonGLPKSolver:
     cdef glp_prob* prob
     cdef glp_smcp smcp
     cdef int idx_col_routes
-    cdef int idx_col_demands
-    cdef int idx_row_supplys
-    cdef int idx_row_demands
+    cdef int idx_row_non_storages
     cdef int idx_row_cross_domain
     cdef int idx_row_storages
 
     cdef object routes
-    cdef object supplys
-    cdef object demands
+    cdef object non_storages
     cdef object storages
 
     def __cinit__(self):
@@ -40,42 +37,35 @@ cdef class CythonGLPKSolver:
         cdef double max_flow
         cdef double cost
         cdef double avail_volume
-        cdef int col
+        cdef int col, row
         cdef int* ind
         cdef double* val
         cdef double lb
         cdef double ub
         cdef Timestep timestep
         cdef int status
-        cdef cross_domain_col
+        cdef cross_domain_row
 
         routes = model.find_all_routes(BaseInput, BaseOutput, valid=(BaseLink, BaseInput, BaseOutput))
         # Find cross-domain routes
         cross_domain_routes = model.find_all_routes(BaseOutput, BaseInput, max_length=2, domain_match='different')
 
-
-        supplys = []
-        demands = []
+        non_storages = []
         storages = []
         for some_node in model.nodes():
-            if isinstance(some_node, (BaseInput, BaseLink)):
-                supplys.append(some_node)
-            if isinstance(some_node, BaseOutput):
-                demands.append(some_node)
+            if isinstance(some_node, (BaseInput, BaseLink, BaseOutput)):
+                non_storages.append(some_node)
             if isinstance(some_node, Storage):
                 storages.append(some_node)
 
         assert(routes)
-        assert(supplys)
-        assert(demands)
+        assert(non_storages)
 
         # clear the previous problem
         glp_erase_prob(self.prob)
         glp_set_obj_dir(self.prob, GLP_MIN)
         # add a column for each route
         self.idx_col_routes = glp_add_cols(self.prob, <int>(len(routes)))
-        # add a column for each demand
-        self.idx_col_demands = glp_add_cols(self.prob, <int>(len(demands)))
 
         # create a lookup for the cross-domain routes.
         cross_domain_cols = {}
@@ -95,66 +85,59 @@ cdef class CythonGLPKSolver:
         # explicitly set bounds on route and demand columns
         for col, route in enumerate(routes):
             glp_set_col_bnds(self.prob, self.idx_col_routes+col, GLP_LO, 0.0, DBL_MAX)
-        for col, demand in enumerate(demands):
-            glp_set_col_bnds(self.prob, self.idx_col_demands+col, GLP_LO, 0.0, DBL_MAX)
 
         # constrain supply minimum and maximum flow
-        self.idx_row_supplys = glp_add_rows(self.prob, len(supplys))
-        for col, supply in enumerate(supplys):
-            # TODO is this a bit hackish??
-            if isinstance(supply, BaseInput):
-                cols = [n for n, route in enumerate(routes) if route[0] is supply]
+        self.idx_row_non_storages = glp_add_rows(self.prob, len(non_storages))
+        # Add rows for the cross-domain routes.
+        if len(cross_domain_cols) > 0:
+            self.idx_row_cross_domain = glp_add_rows(self.prob, len(cross_domain_cols))
+
+        cross_domain_row = 0
+        for row, some_node in enumerate(non_storages):
+            # Differentiate betwen the node type.
+            # Input & Output only apply their flow constraints when they
+            # are the first and last node on the route respectively.
+            if isinstance(some_node, BaseInput):
+                cols = [n for n, route in enumerate(routes) if route[0] is some_node]
+            elif isinstance(some_node, BaseOutput):
+                cols = [n for n, route in enumerate(routes) if route[-1] is some_node]
             else:
-                cols = [n for n, route in enumerate(routes) if supply in route]
+                # Other nodes apply their flow constraints to all routes passing through them
+                cols = [n for n, route in enumerate(routes) if some_node in route]
             ind = <int*>malloc((1+len(cols)) * sizeof(int))
             val = <double*>malloc((1+len(cols)) * sizeof(double))
             for n, c in enumerate(cols):
                 ind[1+n] = 1+c
                 val[1+n] = 1
-            glp_set_mat_row(self.prob, self.idx_row_supplys+col, len(cols), ind, val)
-            glp_set_row_bnds(self.prob, self.idx_row_supplys+col, GLP_FX, 0.0, 0.0)
+            glp_set_mat_row(self.prob, self.idx_row_non_storages+row, len(cols), ind, val)
+            glp_set_row_bnds(self.prob, self.idx_row_non_storages+row, GLP_FX, 0.0, 0.0)
 
-        # link supply and demand variables
-        self.idx_row_demands = glp_add_rows(self.prob, len(demands))
-        if len(cross_domain_cols) > 0:
-            self.idx_row_cross_domain = glp_add_rows(self.prob, len(cross_domain_cols))
-        cross_domain_col = 0
-        for col, demand in enumerate(demands):
-            cols = [n for n, route in enumerate(routes) if route[-1] is demand]
-            ind = <int*>malloc((1+len(cols)+1) * sizeof(int))
-            val = <double*>malloc((1+len(cols)+1) * sizeof(double))
-            for n, c in enumerate(cols):
-                ind[1+n] = 1+c
-                val[1+n] = 1
-            ind[1+len(cols)] = self.idx_col_demands+col
-            val[1+len(cols)] = -1
-            glp_set_mat_row(self.prob, self.idx_row_demands+col, len(cols)+1, ind, val)
-            glp_set_row_bnds(self.prob, self.idx_row_demands+col, GLP_FX, 0.0, 0.0)
             # Add constraint for cross-domain routes
             # i.e. those from a demand to a supply
-            if demand in cross_domain_cols:
-                col_vals = cross_domain_cols[demand]
-                ind = <int*>malloc((1+len(col_vals)+1) * sizeof(int))
-                val = <double*>malloc((1+len(col_vals)+1) * sizeof(double))
-                for n, (c, v) in enumerate(col_vals):
+            if some_node in cross_domain_cols:
+                col_vals = cross_domain_cols[some_node]
+                ind = <int*>malloc((1+len(col_vals)+len(cols)) * sizeof(int))
+                val = <double*>malloc((1+len(col_vals)+len(cols)) * sizeof(double))
+                for n, c in enumerate(cols):
                     ind[1+n] = 1+c
-                    val[1+n] = 1./v
-                ind[1+len(col_vals)] = self.idx_col_demands+col
-                val[1+len(col_vals)] = -1
-                glp_set_mat_row(self.prob, self.idx_row_cross_domain+cross_domain_col, len(col_vals)+1, ind, val)
-                glp_set_row_bnds(self.prob, self.idx_row_cross_domain+cross_domain_col, GLP_FX, 0.0, 0.0)
-                cross_domain_col += 1
+                    val[1+n] = -1
+                for n, (c, v) in enumerate(col_vals):
+                    ind[1+n+len(cols)] = 1+c
+                    val[1+n+len(cols)] = 1./v
+                glp_set_mat_row(self.prob, self.idx_row_cross_domain+cross_domain_row, len(col_vals)+len(cols), ind, val)
+                glp_set_row_bnds(self.prob, self.idx_row_cross_domain+cross_domain_row, GLP_FX, 0.0, 0.0)
+                cross_domain_row += 1
 
         # storage
         if len(storages):
             self.idx_row_storages = glp_add_rows(self.prob, len(storages))
         for col, storage in enumerate(storages):
-            cols_output = [n for n, demand in enumerate(demands) if demand in storage.outputs]
+            cols_output = [n for n, route in enumerate(routes) if route[-1] in storage.outputs]
             cols_input = [n for n, route in enumerate(routes) if route[0] in storage.inputs]
             ind = <int*>malloc((1+len(cols_output)+len(cols_input)) * sizeof(int))
             val = <double*>malloc((1+len(cols_output)+len(cols_input)) * sizeof(double))
             for n, c in enumerate(cols_output):
-                ind[1+n] = self.idx_col_demands+c
+                ind[1+n] = self.idx_col_routes+c
                 val[1+n] = 1
             for n, c in enumerate(cols_input):
                 ind[1+len(cols_output)+n] = self.idx_col_routes+c
@@ -162,8 +145,7 @@ cdef class CythonGLPKSolver:
             glp_set_mat_row(self.prob, self.idx_row_storages+col, len(cols_output)+len(cols_input), ind, val)
 
         self.routes = routes
-        self.supplys = supplys
-        self.demands = demands
+        self.non_storages = non_storages
         self.storages = storages
 
     cpdef object solve(self, model):
@@ -173,8 +155,6 @@ cdef class CythonGLPKSolver:
             self._solve_scenario(model, scenario_id, scenario_combination)
 
     cdef object _solve_scenario(self, model, int scenario_id, int[:] scenario_indices):
-        cdef Node supply
-        cdef Node demand
         cdef Node node
         cdef double min_flow
         cdef double max_flow
@@ -191,30 +171,21 @@ cdef class CythonGLPKSolver:
 
         timestep = model.timestep
         routes = self.routes
-        supplys = self.supplys
-        demands = self.demands
+        non_storages = self.non_storages
         storages = self.storages
 
         # update route properties
         for col, route in enumerate(routes):
             cost = 0.0
-            for node in route[:-1]:
+            for node in route:
                 cost += node.get_cost(timestep, scenario_indices)
             glp_set_obj_coef(self.prob, self.idx_col_routes+col, cost)
 
-        # update supply properties
-        for col, supply in enumerate(supplys):
-            min_flow = inf_to_dbl_max(supply.get_min_flow(timestep, scenario_indices))
-            max_flow = inf_to_dbl_max(supply.get_max_flow(timestep, scenario_indices))
-            glp_set_row_bnds(self.prob, self.idx_row_supplys+col, constraint_type(min_flow, max_flow), min_flow, max_flow)
-
-        # update demand properties
-        for col, demand in enumerate(demands):
-            min_flow = inf_to_dbl_max(demand.get_min_flow(timestep, scenario_indices))
-            max_flow = inf_to_dbl_max(demand.get_max_flow(timestep, scenario_indices))
-            cost = demand.get_cost(timestep, scenario_indices)
-            glp_set_col_bnds(self.prob, self.idx_col_demands+col, constraint_type(min_flow, max_flow), min_flow, max_flow)
-            glp_set_obj_coef(self.prob, self.idx_col_demands+col, cost)
+        # update non-storage properties
+        for col, node in enumerate(non_storages):
+            min_flow = inf_to_dbl_max(node.get_min_flow(timestep, scenario_indices))
+            max_flow = inf_to_dbl_max(node.get_max_flow(timestep, scenario_indices))
+            glp_set_row_bnds(self.prob, self.idx_row_non_storages+col, constraint_type(min_flow, max_flow), min_flow, max_flow)
 
         # update storage node constraint
         for col, storage in enumerate(storages):
