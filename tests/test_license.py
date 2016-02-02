36c371db40e663cc372bcc8e90001b9b3f2f15d7
James Tomlinson <tomo.bbe@gmail.com>
2016-02-02 22:06:03 +0000

    Use assert_allclose in test_licences.

--- tests/test_license.py
+++ tests/test_license.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from pywr._core import Timestep
 from pywr.licenses import License, TimestepLicense, AnnualLicense
 from fixtures import simple_linear_model
-
+from numpy.testing import assert_allclose
 
 def test_base_license():
     with pytest.raises(TypeError):
@@ -34,29 +34,29 @@ def test_simple_model_with_annual_licence(simple_linear_model):
     m.setup()
     print(lic)
     # timestepper.current is the next time step (because model has not begun)
-    assert lic.value(m.timestepper.current) == annual_total/365
+    assert_allclose(lic.value(m.timestepper.current), annual_total/365)
     m.step()
 
     # Licence is a hard constraint of 1.0
     # timestepper.current is no end of the first day
-    assert m.node["Output"].flow == 1.0
+    assert_allclose(m.node["Output"].flow, 1.0)
     # Check the constraint for the next timestep.
-    assert lic.value(m.timestepper._next) == 1.0
+    assert_allclose(lic.value(m.timestepper._next), 1.0)
 
     # Now constrain the demand so that licence is not fully used
     m.node["Output"].max_flow = 0.5
     m.step()
 
-    assert m.node["Output"].flow == 0.5
+    assert_allclose(m.node["Output"].flow, 0.5)
     # Check the constraint for the next timestep. The available amount should now be large
     # due to the reduced use
     remaining = (annual_total-1.5)
-    assert lic.value(m.timestepper._next) == remaining / (365 - 2)
+    assert_allclose(lic.value(m.timestepper._next), remaining / (365 - 2))
 
     # Unconstrain the demand
     m.node["Output"].max_flow = 10.0
     m.step()
-    assert m.node["Output"].flow == remaining / (365 - 2)
+    assert_allclose(m.node["Output"].flow, remaining / (365 - 2))
     # Licence should now be on track for an expected value of 1.0
     remaining -= remaining / (365 - 2)
-    assert lic.value(m.timestepper._next) == remaining / (365 - 3)
+    assert_allclose(lic.value(m.timestepper._next), remaining / (365 - 3))
