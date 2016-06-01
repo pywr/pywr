066c9f56fd1939f5b8d2c7c4d101eeffa189b2d3
Joshua Arnott <josh@snorfalorpagus.net>
2016-06-01 12:12:39 +0100

    KeatingStorage can now be created with more than one stream outflow

--- pywr/domains/groundwater.py
+++ pywr/domains/groundwater.py
@@ -4,15 +4,53 @@ from ..parameters.groundwater import KeatingStreamFlowParameter
 
 
 class KeatingAquifer(Storage):
-    def __init__(self, model, name, levels, volumes, stream_flow_levels, transmissivity, coefficient=1.0, **kwargs):
-        super(KeatingAquifer, self).__init__(model, name, **kwargs)
+    def __init__(self, model, name,
+                 num_streams, num_additional_inputs,
+                 levels, volumes,
+                 stream_flow_levels, transmissivity, coefficient,
+                 **kwargs):
+        """Storage node with one or more Keating outflows
+
+        Parameters
+        ----------
+        model : pywr.core.Model
+            The Pywr Model.
+        name : string
+            A unique name for the node in the model.
+        num_streams : integer
+            Number of keating outflows.
+        num_additional_inputs : integer
+            Number of additional outflows (e.g. for direct abstraction or
+            discharge from the aquifer).
+        levels : list of floats
+            A list of levels for the level-volume relationship. The length
+            should be greater than 1.
+        volumes : list of floats
+            A list of volumes for the level-volume relationship. The length
+            should be the same as `levels`.
+        stream_flow_levels : list of list of floats
+            For each stream a list of levels to pass to the keating streamflow
+            parameter.
+        transmissivity : list of list of floats
+            For each stream a list of transmissivity values to pass to the
+            keating streamflow parameter.
+        coefficient : list of list of floats
+            For each stream a list of coefficients to pass to the keating
+            streamflow parameter.
+
+        See also documentation for the `KeatingStreamFlowParameter`.
+        """
+        super(KeatingAquifer, self).__init__(model, name,
+            num_inputs=(num_streams + num_additional_inputs), **kwargs)
 
         self.max_volume = max(volumes)
         self.min_volume = min(volumes)
         self.level = InterpolatedLevelParameter(volumes, levels)
-        # Make the first StorageInput node the aquifer outflow
-        inpt = self.inputs[0]
-        param = KeatingStreamFlowParameter(self, stream_flow_levels, transmissivity, coefficient=coefficient)
-        # Ensure the StorageInput flows out the prescribed amount.
-        inpt.max_flow = param
-        inpt.min_flow = param
+
+        # initialise streamflow parameters
+        for n, node in enumerate(self.inputs[0:num_streams]):
+            parameter = KeatingStreamFlowParameter(self, stream_flow_levels[n],
+                                                   transmissivity[n],
+                                                   coefficient[n])
+            node.max_flow = parameter
+            node.min_flow = parameter
