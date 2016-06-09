a3a4a6f5be021758a45f12871f4af4f95e13853f
Joshua Arnott <josh@snorfalorpagus.net>
2016-06-09 16:09:53 +0100

    Added tests, docstring and ability to set initial level directly.

--- pywr/domains/groundwater.py
+++ pywr/domains/groundwater.py
@@ -2,12 +2,14 @@ from ..core import Storage
 from ..parameters import InterpolatedLevelParameter
 from ..parameters.groundwater import KeatingStreamFlowParameter
 
+from scipy.interpolate import interp1d
+
 
 class KeatingAquifer(Storage):
     def __init__(self, model, name,
                  num_streams, num_additional_inputs,
-                 levels, volumes,
                  stream_flow_levels, transmissivity, coefficient,
+                 levels, volumes=None, area=None, storitivity=None,
                  **kwargs):
         """Storage node with one or more Keating outflows
 
@@ -22,12 +24,6 @@ class KeatingAquifer(Storage):
         num_additional_inputs : integer
             Number of additional outflows (e.g. for direct abstraction or
             discharge from the aquifer).
-        levels : list of floats
-            A list of levels for the level-volume relationship. The length
-            should be greater than 1.
-        volumes : list of floats
-            A list of volumes for the level-volume relationship. The length
-            should be the same as `levels`.
         stream_flow_levels : list of list of floats
             For each stream a list of levels to pass to the keating streamflow
             parameter.
@@ -35,11 +31,59 @@ class KeatingAquifer(Storage):
             The transmissivity for each stream flow level.
         coefficient : list of floats
             The coefficient for each stream flow level.
+        levels : list of floats
+            A list of levels for the level-volume relationship. The length
+            should be greater than 1.
+        volumes : list of floats (optional)
+            A list of volumes for the level-volume relationship. The length
+            should be the same as `levels`.
+        area : float (optional)
+            Area of the aquifer in m2.
+        storitivity : list of floats (optional)
+            Storitivity of the aquifer as a factor (e.g. 0.05). This defines
+            part of the volume-level relationship. The length should be one
+            less than `levels`.
+
+        Either supply the `volumes` argument or both the `area` and
+        `storitivity` arguments.
 
         See also documentation for the `KeatingStreamFlowParameter`.
         """
         super(KeatingAquifer, self).__init__(model, name,
             num_inputs=(num_streams + num_additional_inputs), **kwargs)
+
+        if not (num_streams > 0):
+            raise ValueError("Keating aquifer must have at least one stream outflow")
+        if len(stream_flow_levels) != num_streams:
+            raise ValueError("Stream flow levels must have `num_streams` items")
+        for i in stream_flow_levels:
+            if len(i) != len(transmissivity):
+                raise ValueError("Items in stream flow levels should have the same length as transmissivity")
+        if not isinstance(coefficient, (int, float)):
+            raise ValueError("Coefficient must be a scalar")
+
+        if volumes is None:
+            if not isinstance(area, (int, float)):
+                raise ValueError("Area must be a scalar")
+            if len(storitivity) != (len(levels) - 1):
+                raise ValueError("Storitivity must have one less item than levels")
+            heights = [levels[n+1] - levels[n] for n in range(0, len(levels)-1)]
+            volumes = [0.0]
+            for n, (s, h) in enumerate(zip(storitivity, heights)):
+                volumes.append(volumes[-1] + area * s * h * 0.001)
+        else:
+            # check volumes
+            if len(volumes) != len(levels):
+                raise ValueError("Volumes must have the same length as levels")
+
+        self.area = area
+
+        if len(levels) != len(volumes):
+            raise ValueError("Levels and volumes must have the same length")
+
+        self._volumes = volumes
+        self._levels = levels
+        self._level_to_volume = interp1d(levels, volumes)
 
         self.max_volume = max(volumes)
         self.min_volume = min(volumes)
@@ -52,3 +96,14 @@ class KeatingAquifer(Storage):
                                                    coefficient)
             node.max_flow = parameter
             node.min_flow = parameter
+
+    def initial_level():
+        def fget(self):
+            # get the initial level from the volume
+            return self.level.interp(self.initial_volume)
+        def fset(self, value):
+            # actually sets the initial volume
+            volume = self._level_to_volume(value)
+            self.initial_volume = volume
+        return locals()
+    initial_level = property(**initial_level())
