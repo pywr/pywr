dd5b7e0a0148e6bb59632a2c1a7fd304399b4625
Joshua Arnott <josh@snorfalorpagus.net>
2016-06-30 15:14:18 +0100

    Added some workarounds for IE in draw_graph.js

--- pywr/notebook/draw_graph.js
+++ pywr/notebook/draw_graph.js
@@ -1,3 +1,14 @@
+// javascript jinja2 template for drawing a directional graph
+
+// internet explorer can't follow standards so we need some workarounds...
+var ua = window.navigator.userAgent;
+var browser;
+if ((ua.indexOf("MSIE") != -1) || (ua.indexOf("Trident") != -1)) {
+    browser = "ie";
+} else {
+    browser = "something better than ie";
+}
+
 var graph = {{ graph }};
 
 var style = d3.selectAll(element).append("style");
@@ -14,6 +25,10 @@ var force = d3.layout.force()
     .charge(-120)
     .linkDistance(25)
     .size([width, height]);
+
+// workaround for IE not sizing output correctly
+div.style("height", height+"px")
+   .style("width", width+"px");
 
 var svg = div.append("svg")
     .attr("width", width)
@@ -36,14 +51,25 @@ svg.append("svg:defs").append("svg:marker")
   .attr("d", "M0,-5L10,0L0,5")
   .attr("fill", "#333");
 
-var link = svg.selectAll(".link")
-  .data(graph.links)
-.enter().append("svg:path")
-  .attr("class", "link")
-  .style("fill", "none")
-  .style("stroke", "#333")
-  .style("stroke-width", 2)
-  .style("marker-end", function(d) { return "url(#end-arrow)"; });
+if(browser == "ie") {
+    // internet explorer
+    var link = svg.selectAll(".link")
+      .data(graph.links)
+    .enter().append("line")
+      .attr("class", "link")
+      .style("stroke", "#333")
+      .style("stroke-width", 2);
+} else {
+    // firefox, etc.
+    var link = svg.selectAll(".link")
+      .data(graph.links)
+    .enter().append("svg:path")
+      .attr("class", "link")
+      .style("fill", "none")
+      .style("stroke", "#333")
+      .style("stroke-width", 2)
+      .style("marker-end", function(d) { return "url(#end-arrow)"; });
+}
 
 var node_size = 5;
 
@@ -65,20 +91,29 @@ node.append("title")
   .text(function(d) { return d.name; });
 
 function tick() {
-    link.attr("d", function(d) {
-        var deltaX = d.target.x - d.source.x,
-            deltaY = d.target.y - d.source.y,
-            dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
-            normX = deltaX / dist,
-            normY = deltaY / dist,
-            sourcePadding = node_size,
-            targetPadding = node_size + 3,
-            sourceX = d.source.x + (sourcePadding * normX),
-            sourceY = d.source.y + (sourcePadding * normY),
-            targetX = d.target.x - (targetPadding * normX),
-            targetY = d.target.y - (targetPadding * normY);
-        return "M" + sourceX + "," + sourceY + "L" + targetX + "," + targetY;
-    });
+    if(browser == "ie") {
+        // internet explorer
+        link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+    } else {
+        // firefox, etc.
+        link.attr("d", function(d) {
+            var deltaX = d.target.x - d.source.x,
+                deltaY = d.target.y - d.source.y,
+                dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
+                normX = deltaX / dist,
+                normY = deltaY / dist,
+                sourcePadding = node_size,
+                targetPadding = node_size + 3,
+                sourceX = d.source.x + (sourcePadding * normX),
+                sourceY = d.source.y + (sourcePadding * normY),
+                targetX = d.target.x - (targetPadding * normX),
+                targetY = d.target.y - (targetPadding * normY);
+            return "M" + sourceX + "," + sourceY + "L" + targetX + "," + targetY;
+        });
+    }
 
     node.attr("transform", function(d) {
         return "translate(" + d.x + "," + d.y + ")";
