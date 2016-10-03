dc37a641f3f74f8b9d381274bbc127e1911d8394
Joshua Arnott <josh@snorfalorpagus.net>
2016-10-03 18:15:14 +0100

    Added --single-version-externally-managed

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -2,5 +2,5 @@ set LIB=%LIBRARY_LIB%;.\lib;%LIB%
 set LIBPATH=%LIBRARY_LIB%;.\lib;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 
-"%PYTHON%" setup.py build_ext -I"%LIBRARY_INC%" -L"%LIBRARY_LIB%" install --with-glpk --with-lpsolve
+"%PYTHON%" setup.py build_ext -I"%LIBRARY_INC%" -L"%LIBRARY_LIB%" install --with-glpk --with-lpsolve --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
