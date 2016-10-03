5a4179649c3b354583cf5e72d68cbff414680ec7
Joshua Arnott <josh@snorfalorpagus.net>
2016-10-03 18:15:14 +0100

    More path fixes

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -1,3 +1,3 @@
 py.test %SRC_DIR%\tests --solver=glpk
 py.test %SRC_DIR%\tests --solver=lpsolve
-jupyter nbconvert --to html --execute tests\notebook.ipynb
+jupyter nbconvert --to html --execute %SRC_DIR%\tests\notebook.ipynb
