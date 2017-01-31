73f313d380242e83f5f83327194a2f3e13ecb392
James Tomlinson <tomo.bbe@gmail.com>
2017-01-31 21:35:41 +0000

    Storage current_pc now accounts for min_volume != 0

--- pywr/_core.pyx
+++ pywr/_core.pyx
@@ -792,6 +792,7 @@ cdef class Storage(AbstractStorage):
     cpdef _reset_storage_only(self):
         cdef int i
         cdef double mxv = self._max_volume
+        cdef double mnv = self._min_volume
         cdef ScenarioIndex si
 
         for i, si in enumerate(self.model.scenarios.combinations):
@@ -799,8 +800,11 @@ cdef class Storage(AbstractStorage):
             # Ensure variable maximum volume is taken in to account
             if self._max_volume_param is not None:
                 mxv = self._max_volume_param.value(self.model.timestepper.current, si)
+            # Ensure variable minimum volume is taken in to account
+            if self._min_volume_param is not None:
+                mnv = self._min_volume_param.value(self.model.timestepper.current, si)
             try:
-                self._current_pc[i] = self._volume[i] / mxv
+                self._current_pc[i] = self._volume[i] / (mxv - mnv)
             except ZeroDivisionError:
                 self._current_pc[i] = np.nan
 
@@ -820,6 +824,7 @@ cdef class Storage(AbstractStorage):
         AbstractStorage.after(self, ts)
         cdef int i
         cdef double mxv = self._max_volume
+        cdef double mnv = self._min_volume
         cdef ScenarioIndex si
 
         if self._cost_param is not None:
@@ -834,8 +839,11 @@ cdef class Storage(AbstractStorage):
             # Ensure variable maximum volume is taken in to account
             if self._max_volume_param is not None:
                 mxv = self._max_volume_param.value(self.model.timestepper.current, si)
+            # Ensure variable minimum volume is taken in to account
+            if self._min_volume_param is not None:
+                mnv = self._min_volume_param.value(self.model.timestepper.current, si)
             try:
-                self._current_pc[i] = self._volume[i] / mxv
+                self._current_pc[i] = self._volume[i] / (mxv - mnv)
             except ZeroDivisionError:
                 self._current_pc[i] = np.nan
 
