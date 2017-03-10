0d03bd5472dcbf7dab74e65fa79ef4218f5c10a4
James Tomlinson <tomo.bbe@gmail.com>
2017-03-10 22:09:56 +0000

    Fix TotalDeficitNodeRecorder to multiply by time-step length.
    Fixes #433

--- pywr/recorders/_recorders.pyx
+++ pywr/recorders/_recorders.pyx
@@ -804,10 +804,11 @@ cdef class TotalDeficitNodeRecorder(BaseConstantNodeRecorder):
         cdef double max_flow
         cdef ScenarioIndex scenario_index
         cdef Timestep ts = self.model.timestepper.current
+        cdef int days = self.model.timestepper.delta.days
         cdef AbstractNode node = self._node
         for scenario_index in self.model.scenarios.combinations:
             max_flow = node.get_max_flow(ts, scenario_index)
-            self._values[scenario_index._global_id] += max_flow - node._flow[scenario_index._global_id]
+            self._values[scenario_index._global_id] += (max_flow - node._flow[scenario_index._global_id])*days
 
         return 0
 TotalDeficitNodeRecorder.register()
