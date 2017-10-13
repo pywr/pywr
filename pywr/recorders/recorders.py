00b2b214797c1c0c6fd0863b71d5416684a03ae5
Joshua Arnott <josh@snorfalorpagus.net>
2017-10-13 17:55:21 +0100

    Fix for bz2 csv in Python 2.x

--- pywr/recorders/recorders.py
+++ pywr/recorders/recorders.py
@@ -142,7 +142,13 @@ class CSVRecorder(Recorder):
             self._fh = gzip.open(self.csvfile, "wt", self.complevel, **kwargs)
         elif self.complib in ("bz2", "bzip2"):
             import bz2
-            self._fh = bz2.open(self.csvfile, "wt", self.complevel, **kwargs)
+            import io
+            if sys.version_info.major >= 3:
+                self._fh = bz2.open(self.csvfile, "wt", self.complevel, **kwargs)
+            else:
+                fh = bz2.BZ2File(self.csvfile, "wb", self.complevel)
+                self._fh = io.TextIOWrapper(fh, **kwargs)
+                self._fh.close = fh.close
         elif self.complib is None:
             self._fh = open(self.csvfile, "w", **kwargs)
         else:
