da5143f86aa29188041de72b536cf1941f836174
James Tomlinson <tomo.bbe@gmail.com>
2017-12-10 19:45:33 +0000

    Extended the TransientDecisionParameter to support is_variable=True.

--- pywr/parameters/transient.py
+++ pywr/parameters/transient.py
@@ -4,6 +4,7 @@ Examples include the modelling of a decision at a fixed point during a simulatio
 
 """
 from ._parameters import Parameter
+import numpy as np
 import pandas
 
 
@@ -20,11 +21,20 @@ class TransientDecisionParameter(Parameter):
         The trigger date for the decision.
     before_parameter : Parameter
         The value to use before the decision date.
-    after_parameter: Parameter
+    after_parameter : Parameter
         The value to use after the decision date.
+    earliest_date : string or pandas.Timestamp or None
+        Earliest date that the variable can be set to. Defaults to `model.timestepper.start`
+    latest_date : string or pandas.Timestamp or None
+        Latest date that the variable can be set to. Defaults to `model.timestepper.end`
+    decision_freq : pandas frequency string (default 'AS')
+        The resolution of feasible dates. For example 'AS' would create feasible dates every
+        year between `earliest_date` and `latest_date`. The `pandas` functions are used
+        internally for delta date calculations.
 
     """
-    def __init__(self, model, decision_date, before_parameter, after_parameter, **kwargs):
+    def __init__(self, model, decision_date, before_parameter, after_parameter, 
+                 earliest_date=None, latest_date=None, decision_freq='AS', **kwargs):
         super(TransientDecisionParameter, self).__init__(model, **kwargs)
         self._decision_date = None
         self.decision_date = decision_date
@@ -38,6 +48,17 @@ class TransientDecisionParameter(Parameter):
             raise ValueError('The `after` value should be a Parameter instance.')
         after_parameter.parents.add(self)
         self.after_parameter = after_parameter
+        
+        # These parameters are mostly used if this class is used as variable.
+        self._earliest_date = None
+        self.earliest_date = earliest_date
+        
+        self._latest_date = None
+        self.latest_date = latest_date
+
+        self.decision_freq = decision_freq
+        self._feasible_dates = None
+        self.size = 1  # This parameter is always size 1
 
     def decision_date():
         def fget(self):
@@ -49,6 +70,41 @@ class TransientDecisionParameter(Parameter):
                 self._decision_date = pandas.to_datetime(value)
         return locals()
     decision_date = property(**decision_date())
+    
+    def earliest_date():
+        def fget(self):
+            if self._earliest_date is not None:
+                return self._earliest_date
+            else:
+                return self.model.timestepper.start
+        def fset(self, value):
+            if isinstance(value, pandas.Timestamp):
+                self._earliest_date = value
+            else:
+                self._earliest_date = pandas.to_datetime(value)
+        return locals()
+    earliest_date = property(**earliest_date())
+    
+    def latest_date():
+        def fget(self):
+            if self._latest_date is not None:
+                return self._latest_date
+            else:
+                return self.model.timestepper.end
+        def fset(self, value):
+            if isinstance(value, pandas.Timestamp):
+                self._latest_date = value
+            else:
+                self._latest_date = pandas.to_datetime(value)
+        return locals()
+    latest_date = property(**latest_date())
+
+    def setup(self):
+        super(TransientDecisionParameter, self).setup()
+
+        # Now setup the feasible dates for when this object is used as a variable.
+        self._feasible_dates = pandas.date_range(self.earliest_date, self.latest_date,
+                                                 freq=self.decision_freq)
 
     def value(self, ts, scenario_index):
 
@@ -57,6 +113,17 @@ class TransientDecisionParameter(Parameter):
         else:
             v = self.before_parameter.get_value(scenario_index)
         return v
+
+    def lower_bounds(self):
+        return np.array([0.0, ])
+
+    def upper_bounds(self):
+        return np.array([len(self._feasible_dates)-1, ], dtype=np.float64)
+
+    def update(self, values):
+        # Update the decision date with the corresponding feasible date
+        self.decision_date = self._feasible_dates[int(round(values[0]))]
+
 
 
             
--- tests/test_parameters_transient.py
+++ tests/test_parameters_transient.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas
 from pywr.recorders import AssertionRecorder, assert_rec
 from fixtures import simple_linear_model, simple_storage_model
+from test_parameters import model
 
 
 class TestTransientDecisionParameter:
@@ -28,3 +29,54 @@ class TestTransientDecisionParameter:
                 return after.get_value(scenario_index)
 
         model.run()
+
+    def test_start_end_dates(self, model):
+        """ Test the assignment of earliest_date and latest_date keywords """
+        date = '2015-02-01'
+        before = ConstantParameter(model, 0)
+        after = ConstantParameter(model, 1)
+
+        # Default to model start and end dates
+        p = TransientDecisionParameter(model, date, before, after)
+
+        assert p.earliest_date == model.timestepper.start
+        assert p.latest_date == model.timestepper.end
+
+        # Test with user defined start and end dates
+        p = TransientDecisionParameter(model, date, before, after, earliest_date='2015-02-01',
+                                       latest_date='2020-02-03')
+
+        assert p.earliest_date == pandas.to_datetime('2015-02-01')
+        assert p.latest_date == pandas.to_datetime('2020-02-03')
+
+    def test_variable_setup(self, simple_linear_model):
+        """ Test the setup of the feasible dates for when using as a variable.
+
+         This example should create 6 feasible dates at annual year start frequency.
+         """
+        model = simple_linear_model
+
+        # This model run is 5 year; it should create 6 possible dates
+        model.timestepper.start = '2015-01-01'
+        model.timestepper.end = '2020-01-01'
+
+        date = '2017-01-01'
+        before = ConstantParameter(model, 0)
+        after = ConstantParameter(model, 1)
+
+        # The decision resolution is annual
+        # With this setup the model should have 5 possible values
+        p = TransientDecisionParameter(model, date, before, after)
+
+        model.setup()
+
+        np.testing.assert_allclose(p.lower_bounds(), [0.0, ])
+        np.testing.assert_allclose(p.upper_bounds(), [5.0, ])  # 6th index
+        for i, feasible_date in enumerate(p._feasible_dates):
+            assert feasible_date == model.timestepper.start + pandas.DateOffset(years=i)
+
+        # Finally check updating the variable works as expected
+        p.update(np.array([0.8, ]))
+        assert p.decision_date == pandas.to_datetime('2016-01-01')
+
+
