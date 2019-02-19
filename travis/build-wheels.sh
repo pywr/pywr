999922f799b0af636b2d5973d16bc55d19c1e431
James Tomlinson <tomo.bbe@gmail.com>
2019-02-19 15:57:35 +0000

    Refactor .travis.yml and build-wheels.sh.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,21 +3,14 @@
 # Note this script does not build the wheels with lpsolve support.
 set -e -x
 
-yum install -y glpk glpk-devel
-ln -sf /usr/include/glpk/glpk.h /usr/include/glpk.h
-
-# Support binaries
-PYBINS=( /opt/python/cp36-cp36m/bin /opt/python/cp37-cp37m/bin )
-
 # Compile wheels
-for PYBIN in "${PYBINS[@]}"; do
-    # Install the build dependencies of the project. If some dependencies contain
-    # compiled extensions and are not provided as pre-built wheel packages,
-    # pip will build them from source using the MSVC compiler matching the
-    # target Python version and architecture
-    "${PYBIN}/pip" install cython packaging numpy jupyter pytest wheel
-    PYWR_BUILD_GLPK="true" "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-done
+
+# Install the build dependencies of the project. If some dependencies contain
+# compiled extensions and are not provided as pre-built wheel packages,
+# pip will build them from source using the MSVC compiler matching the
+# target Python version and architecture
+"${PYBIN}/pip" install cython packaging numpy jupyter pytest wheel
+PYWR_BUILD_GLPK="true" "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in ${WORKDIR}/wheelhouse/pywr*.whl; do
@@ -25,8 +18,7 @@ for whl in ${WORKDIR}/wheelhouse/pywr*.whl; do
 done
 
 # Install packages and test
-for PYBIN in "${PYBINS[@]}"; do
-    "${PYBIN}/pip" install pywr --no-index -f /io/wheelhouse
-    "pip install platypus-opt inspyred pygmo"
-    "${PYBIN}/py.test" ${WORKDIR}/pywr/tests
-done
+
+"${PYBIN}/pip" install pywr --no-index -f /io/wheelhouse
+"pip install platypus-opt inspyred pygmo"
+"${PYBIN}/pytest" ${WORKDIR}/pywr/tests
