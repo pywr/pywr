e166e677b59b66571dc53a505a434b3695cd2214
Mohammed Basheer <mohammed.basheer@postgrad.manchester.ac.uk>
2019-06-25 12:07:53 +0100

    Added a coefficient of determination recorder, and added recorders for storage nodes

--- pywr/recorders/calibration.py
+++ pywr/recorders/calibration.py
@@ -12,7 +12,7 @@ Several different evaluation metrics are implemented in this module. Many are
 
 
 """
-from .recorders import NumpyArrayNodeRecorder
+from .recorders import NumpyArrayNodeRecorder, NumpyArrayStorageRecorder
 import numpy as np
 
 
@@ -82,3 +82,84 @@ class NashSutcliffeEfficiencyNodeRecorder(AbstractComparisonNodeRecorder):
         return 1.0 - np.sum((obs-mod)**2, axis=0)/np.sum((obs-obs_mean)**2, axis=0)
 
 
+class CoefficientOfDeterminationNodeRecorder(AbstractComparisonNodeRecorder):
+    """ Recorder evaluates the Coefficient of Determination of model and observed """
+    def values(self):
+        mod = self.data        
+        obs = self._aligned_observed
+        n_items = len(mod)    
+        return ((n_items*np.sum(mod*obs, axis=0)-np.sum(mod, axis=0)*np.sum(obs, axis=0))/(np.sqrt((n_items*np.sum(mod**2, axis=0)-(np.sum(mod, axis=0))**2)*(n_items*np.sum(obs**2, axis=0)-(np.sum(obs, axis=0))**2))))**2
+
+
+class AbstractComparisonStorageRecorder(NumpyArrayStorageRecorder):
+    """ Base class for all Recorders performing timeseries comparison of `Node` storage
+
+
+    """
+    def __init__(self, model, node, observed, **kwargs):
+        super(AbstractComparisonStorageRecorder, self).__init__(model, node, **kwargs)
+        self.observed = observed
+        self._aligned_observed = None
+
+    def setup(self):
+        super(AbstractComparisonStorageRecorder, self).setup()
+        # Align the observed data to the model
+        from pywr.parameters import align_and_resample_dataframe
+        self._aligned_observed = align_and_resample_dataframe(self.observed, self.model.timestepper.datetime_index)
+
+
+class RootMeanSquaredErrorStorageRecorder(AbstractComparisonStorageRecorder):
+    """ Recorder evaluates the RMSE between model and observed """
+    def values(self):
+        mod = self.data
+        obs = self._aligned_observed
+        return np.sqrt(np.mean((obs-mod)**2, axis=0))
+
+
+class MeanAbsoluteErrorStorageRecorder(AbstractComparisonStorageRecorder):
+    """ Recorder evaluates the MAE between model and observed """
+    def values(self):
+        mod = self.data
+        obs = self._aligned_observed
+        return np.mean(np.abs(obs-mod), axis=0)
+
+
+class MeanSquareErrorStorageRecorder(AbstractComparisonStorageRecorder):
+    """ Recorder evaluates the MSE between model and observed """
+    def values(self):
+        mod = self.data
+        obs = self._aligned_observed
+        return np.mean((obs-mod)**2, axis=0)
+
+
+class PercentBiasStorageRecorder(AbstractComparisonStorageRecorder):
+    """ Recorder evaluates the percent bias between model and observed """
+    def values(self):
+        mod = self.data
+        obs = self._aligned_observed
+        return np.sum(obs-mod, axis=0)*100/np.sum(obs, axis=0)
+
+
+class RMSEStandardDeviationRatioStorageRecorder(AbstractComparisonStorageRecorder):
+    """ Recorder evaluates the RMSE-observations standard deviation ratio between model and observed """
+    def values(self):
+        mod = self.data
+        obs = self._aligned_observed
+        return np.sqrt(np.mean((obs-mod)**2, axis=0))/np.std(obs, axis=0)
+
+
+class NashSutcliffeEfficiencyStorageRecorder(AbstractComparisonStorageRecorder):
+    """ Recorder evaluates the Nash-Sutcliffe efficiency model and observed """
+    def values(self):
+        mod = self.data
+        obs = self._aligned_observed
+        obs_mean = np.mean(obs, axis=0)
+        return 1.0 - np.sum((obs-mod)**2, axis=0)/np.sum((obs-obs_mean)**2, axis=0)
+
+class CoefficientOfDeterminationStorageRecorder(AbstractComparisonStorageRecorder):
+    """ Recorder evaluates the Coefficient of Determination of model and observed """
+    def values(self):
+        mod = self.data        
+        obs = self._aligned_observed
+        n_items = len(mod)    
+        return ((n_items*np.sum(mod*obs, axis=0)-np.sum(mod, axis=0)*np.sum(obs, axis=0))/(np.sqrt((n_items*np.sum(mod**2, axis=0)-(np.sum(mod, axis=0))**2)*(n_items*np.sum(obs**2, axis=0)-(np.sum(obs, axis=0))**2))))**2
