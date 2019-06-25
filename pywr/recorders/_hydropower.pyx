ec8beeca718c8fdfca82715a9019961efea631bc
Mohammed Basheer <mohammed.basheer@postgrad.manchester.ac.uk>
2019-06-25 22:09:42 +0100

    Added HydropowerRecorderWithVaribaleTailwater

--- pywr/recorders/_hydropower.pyx
+++ pywr/recorders/_hydropower.pyx
@@ -164,6 +164,125 @@ cdef class HydropowerRecorder(NumpyArrayNodeRecorder):
 HydropowerRecorder.register()
 
 
+cdef class HydropowerRecorderWithVaribaleTailwater(NumpyArrayNodeRecorder):
+    """ Calculates the power production using the hydropower equation
+
+    This recorder saves an array of the hydrpower production in each timestep. It can be converted to a dataframe
+    after a model run has completed. It does not calculate total energy production.
+
+    Parameters
+    ----------
+
+    water_elevation_parameter : Parameter instance (default=None)
+        Elevation of water entering the turbine. The difference of this value with the `turbine_elevation` gives
+        the working head of the turbine.
+    turbine_elevation_parameter : Parameter instance (default=None)
+        Elevation of the turbine itself. The difference between the `water_elevation` and this value gives
+        the working head of the turbine.
+    efficiency : float (default=1.0)
+        The efficiency of the turbine.
+    density : float (default=1000.0)
+        The density of water.
+    flow_unit_conversion : float (default=1.0)
+        A factor used to transform the units of flow to be compatible with the equation here. This
+        should convert flow to units of :math:`m^3/second`
+    energy_unit_conversion : float (default=1e-6)
+        A factor used to transform the units of total energy. Defaults to 1e-6 to return :math:`MW`.
+
+    Notes
+    -----
+    The hydropower calculation uses the following equation.
+
+    .. math:: P = \\rho * g * \\delta H * q
+
+    The flow rate in should be converted to units of :math:`m^3` per day using the `flow_unit_conversion` parameter.
+
+    Head is calculated from the given `water_elevation_parameter` and `turbine_elevation` value. If water elevation
+    is given then head is the difference in elevation between the water and the turbine. If water elevation parameter
+    is `None` then the head is simply the turbine elevation.
+
+
+    See Also
+    --------
+    TotalHydroEnergyRecorder
+    pywr.parameters.HydropowerTargetParameter
+
+    """
+    def __init__(self, model, node, water_elevation_parameter=None, turbine_elevation_parameter=None, efficiency=1.0, density=1000,
+                 flow_unit_conversion=1.0, energy_unit_conversion=1e-6, **kwargs):
+        super(HydropowerRecorderWithVaribaleTailwater, self).__init__(model, node, **kwargs)
+
+        self.water_elevation_parameter = water_elevation_parameter
+        self.turbine_elevation_parameter = turbine_elevation_parameter
+        self.efficiency = efficiency
+        self.density = density
+        self.flow_unit_conversion = flow_unit_conversion
+        self.energy_unit_conversion = energy_unit_conversion
+
+    property water_elevation_parameter:
+        def __get__(self):
+            return self._water_elevation_parameter
+        def __set__(self, parameter):
+            if self._water_elevation_parameter:
+                self.children.remove(self._water_elevation_parameter)
+            self.children.add(parameter)
+            self._water_elevation_parameter = parameter
+
+    property turbine_elevation_parameter:
+        def __get__(self):
+            return self._turbine_elevation_parameter
+        def __set__(self, parameter):
+            if self._turbine_elevation_parameter:
+                self.children.remove(self._turbine_elevation_parameter)
+            self.children.add(parameter)
+            self._turbine_elevation_parameter = parameter         
+
+    cpdef after(self):
+        cdef int i
+        cdef double q, head, power
+        cdef Timestep ts = self.model.timestepper.current
+        cdef ScenarioIndex scenario_index
+        flow = self.node.flow
+
+        for scenario_index in self.model.scenarios.combinations:
+
+            if self._water_elevation_parameter is not None:
+                head = self._water_elevation_parameter.get_value(scenario_index)
+                if self.turbine_elevation_parameter is not None:                    
+                    head -= self.turbine_elevation_parameter.get_value(scenario_index)
+            elif self.turbine_elevation_parameter is not None:
+                head = self.turbine_elevation_parameter.get_value(scenario_index)
+            else:
+                raise ValueError('One or both of storage_node or level must be set.')
+
+            # -ve head is not valid
+            head = max(head, 0.0)
+            # Get the flow from the current node
+            q = self._node._flow[scenario_index.global_id]
+            power = hydropower_calculation(q, head, 0.0, self.efficiency, density=self.density,
+                                             flow_unit_conversion=self.flow_unit_conversion,
+                                             energy_unit_conversion=self.energy_unit_conversion)
+
+            self._data[ts._index, scenario_index.global_id] = power
+
+    @classmethod
+    def load(cls, model, data):
+        from pywr.parameters import load_parameter
+        node = model._get_node_from_ref(model, data.pop("node"))
+        if "water_elevation_parameter" in data:
+            water_elevation_parameter = load_parameter(model, data.pop("water_elevation_parameter"))
+        else:
+            water_elevation_parameter = None
+
+        if "turbine_elevation_parameter" in data:
+            turbine_elevation_parameter = load_parameter(model, data.pop("turbine_elevation_parameter"))
+        else:
+            turbine_elevation_parameter = None
+
+        return cls(model, node, water_elevation_parameter=water_elevation_parameter,turbine_elevation_parameter=turbine_elevation_parameter, **data)
+HydropowerRecorderWithVaribaleTailwater.register()
+
+
 cdef class TotalHydroEnergyRecorder(BaseConstantNodeRecorder):
     """ Calculates the total energy production using the hydropower equation from a model run.
 
