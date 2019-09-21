af18a3de1519a655e825b153ef1563d53217ec64
Mohammed Basheer <mohammed.basheer@postgrad.manchester.ac.uk>
2019-09-21 12:17:02 +0100

    Modified "Build.bat" file

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-set LIBRARY=C:\Users\n22209mb\AppData\Local\Continuum\anaconda3\Library
+set LIBRARY=C:\Users\n22209mb\AppData\Local\Continuum\anaconda3\envs\py36\Library
 
 set LIBRARY_INC=%LIBRARY%\include
 
