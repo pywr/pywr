9fec6bca165113bee1235e1f37c2c42a6a9e0192
James Batchelor <james.batchelor@atkinsglobal.com>
2020-01-26 09:36:47 +0000

    Updated draw_graph.js to use d3.v5

--- pywr/notebook/draw_graph.js
+++ pywr/notebook/draw_graph.js
@@ -1,8 +1,11 @@
 // javascript jinja2 template for drawing a directional graph
 
+
+require.config({paths: {d3: 'https://d3js.org/d3.v5.min'}});
+
 // internet explorer can't follow standards so we need some workarounds...
-var ua = window.navigator.userAgent;
-var browser;
+const ua = window.navigator.userAgent;
+let browser;
 if ((ua.indexOf("MSIE") != -1) || (ua.indexOf("Trident") != -1)) {
     browser = "ie";
     var p = d3.selectAll(element).append("p");
@@ -12,198 +15,217 @@ if ((ua.indexOf("MSIE") != -1) || (ua.indexOf("Trident") != -1)) {
     browser = "something better than ie";
 }
 
-var graph = {{ graph }};
+require(["d3"], function(d3) {
+    const graph = {{ graph }};
 
-var style = d3.selectAll(element).append("style");
-style.html("{{ css }}");
+    const links = graph.links.map(d => Object.create(d));
+    const nodes = graph.nodes.map(d => Object.create(d));
 
-var div = d3.selectAll(element).append("div");
+    const style = d3.selectAll(element).append("style");
+    style.html("{{ css }}");
 
-var width = {{ width }},
-    height = {{ height }};
+    const div = d3.selectAll(element).append("div");
 
-var color = d3.scale.category20();
+    const width = {{ width }},
+        height = {{ height }};
 
-var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(25)
-    .size([width, height]);
+    const simulation = d3.forceSimulation(nodes)
+        .force("link", d3.forceLink(graph.links))
+        .force("charge", d3.forceManyBody().strength(-120))
+        .force('center', d3.forceCenter(width / 2, height / 2))
+        .force("x", d3.forceX())
+        .force("y", d3.forceY());
 
-// workaround for IE not sizing output correctly
-div.style("height", height+"px")
-   .style("width", width+"px");
 
-var svg = div.append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    // workaround for IE not sizing output correctly
+    div.style("height", height+"px")
+       .style("width", width+"px");
 
-var posX = d3.scale.linear()
-    .range([0, width])
-    .domain([-100, 100]);
+    const svg = div.append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-var posY = d3.scale.linear()
-    .range([0, height])
-    .domain([100, -100]); // map-style, +ve is up
+    const posX = d3.scaleLinear()
+        .range([0, width])
+        .domain([-100, 100]);
 
-// set initial node positions 
-for (var i = 0; i < graph.nodes.length; i++) {
-    var node = graph.nodes[i];
-    if (node.position != undefined) {
-        node.x = posX(node.position[0]);
-        node.y = posY(node.position[1]);
-        node.fixed = true;
-    } else {
-        node.fixed = false;
+    const posY = d3.scaleLinear()
+        .range([0, height])
+        .domain([100, -100]); // map-style, +ve is up
+
+    // set initial node positions 
+    for (let i = 0; i < nodes.length; i++) {
+        let node = nodes[i];
+        if (node.position != undefined) {
+            node.x = posX(node.position[0]);
+            node.y = posY(node.position[1]);
+            node.fixed = true;
+        } else {
+            node.fixed = false;
+        }
     }
-}
 
-force
-  .nodes(graph.nodes)
-  .links(graph.links);
+    // define end-arrow svg marker
+    svg.append("svg:defs").append("svg:marker")
+    .attr("id", "end-arrow")
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 6)
+    .attr("markerWidth", 3.5)
+    .attr("markerHeight", 3.5)
+    .attr("orient", "auto")
+    .append("svg:path")
+    .attr("d", "M0,-5L10,0L0,5")
+    .attr("fill", "#333");
 
-// define end-arrow svg marker
-svg.append("svg:defs").append("svg:marker")
-  .attr("id", "end-arrow")
-  .attr("viewBox", "0 -5 10 10")
-  .attr("refX", 6)
-  .attr("markerWidth", 3.5)
-  .attr("markerHeight", 3.5)
-  .attr("orient", "auto")
-.append("svg:path")
-  .attr("d", "M0,-5L10,0L0,5")
-  .attr("fill", "#333");
-
-if(browser == "ie") {
-    // internet explorer
-    var link = svg.selectAll(".link")
-      .data(graph.links)
-    .enter().append("line")
-      .attr("class", "link")
-      .style("stroke", "#333")
-      .style("stroke-width", 2);
-} else {
-    // firefox, etc.
-    var link = svg.selectAll(".link")
-      .data(graph.links)
-    .enter().append("svg:path")
-      .attr("class", "link")
-      .style("fill", "none")
-      .style("stroke", "#333")
-      .style("stroke-width", 2)
-      .style("marker-end", function(d) { return "url(#end-arrow)"; });
-}
-
-var node_size = 5;
-
-function dblclick(d) {
-  d3.select(this).classed("fixed", d.fixed = false);
-}
-
-function dragstart(d) {
-  d3.select(this).classed("fixed", d.fixed = true);
-}
-
-var drag = force.drag()
-    .on("dragstart", dragstart);
-
-var nodes = svg.selectAll(".node")
-              .data(graph.nodes)
-              .enter()
-              .append("g")  
-              .on("dblclick", dblclick)
-              .call(drag);
-
-nodes.append("circle")
-    .attr("class", "node")
-    .attr("r", node_size)
-    .attr("class", function(d) {
-        var clss = "node";
-        for (var i=0; i < d.clss.length; i++) {
-            clss += " node-"+d.clss[i];
-        };
-        return clss;
-    });
-
-{% if labels %}
-nodes.append("text")
-    .attr("dx", 10)
-    .attr("dy", 5)
-    .style("font-weight", 100)
-    .classed("node-text", true)
-    .text(function(d){
-        return d.name
-    });
-{% else %}
-nodes.append("title")
-     .text(function(d) { return d.name; });
-{% endif %}
-
-function tick() {
+    let link;
     if(browser == "ie") {
         // internet explorer
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+        link = svg.selectAll(".link")
+        .data(graph.links)
+        .enter().append("line")
+        .attr("class", "link")
+        .style("stroke", "#333")
+        .style("stroke-width", 2);
     } else {
         // firefox, etc.
-        link.attr("d", function(d) {
-            var deltaX = d.target.x - d.source.x,
-                deltaY = d.target.y - d.source.y,
-                dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
-                normX = deltaX / dist,
-                normY = deltaY / dist,
-                sourcePadding = node_size,
-                targetPadding = node_size + 3,
-                sourceX = d.source.x + (sourcePadding * normX),
-                sourceY = d.source.y + (sourcePadding * normY),
-                targetX = d.target.x - (targetPadding * normX),
-                targetY = d.target.y - (targetPadding * normY);
-            return "M" + sourceX + "," + sourceY + "L" + targetX + "," + targetY;
-        });
+        link = svg.selectAll(".link")
+        .data(graph.links)
+        .enter().append("svg:path")
+        .attr("class", "link")
+        .style("fill", "none")
+        .style("stroke", "#333")
+        .style("stroke-width", 2)
+        .style("marker-end", function(d) { return "url(#end-arrow)"; });
     }
 
-    nodes.attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
-    });
-}
-force.on("tick", tick);
+    function dblclick(d) {
+    d3.select(this).classed("fixed", d.fixed = false);
+    }
 
-force.start();
-
-{% if attributes %}
-    nodes.on("mouseover", function(d){
+    function drag() {
+  
+        function dragstarted(d) {
+          if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+          d.fx = d.x;
+          d.fy = d.y;
+        }
         
-        var table  = d3.selectAll(element)
-                    .append("table")
-                    .classed("table-tooltip", true);
-
-        var thead = table.append('thead')
-        var tbody = table.append('tbody');
+        function dragged(d) {
+          d.fx = d3.event.x;
+          d.fy = d3.event.y;
+        }
         
-        var columns = ["attribute", "value"]
-        thead.append('tr')
-            .selectAll('th')
-            .data(columns).enter()
-            .append('th')
-            .text(function (column) { return column; });
- 
-        var rows = tbody.selectAll('tr')
-                        .data(d["attributes"])
-                        .enter()
-                        .append('tr');
+        function dragended(d) {
+          if (!d3.event.active) simulation.alphaTarget(0);
+          d.fx = null;
+          d.fy = null;
+        }
+        
+        return d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
+      }
 
-        var cells = rows.selectAll('td')
-                        .data(function (d) {
-                            return columns.map(function (column) {
-                            return {column: column, value: d[column]};
-                            });
-                        })
-                        .enter()
-                        .append('td')
-                        .text(function (d) { return d.value; });
+    const node = svg.selectAll(".node")
+                .data(nodes)
+                .enter()
+                .append("g")  
+                .on("dblclick", dblclick)
+                .call(drag(simulation));
 
-    }).on("mouseout", function(){
-        d3.select(".table-tooltip").remove()
-    });
-{% endif %}
+    let node_size = 5;
+
+    node.append("circle")
+        .attr("class", "node")
+        .attr("r", node_size)
+        .attr("class", function(d) {
+            let clss = "node";
+            for (let i=0; i < d.clss.length; i++) {
+                clss += " node-"+d.clss[i];
+            };
+            return clss;
+        });
+
+    {% if labels %}
+    node.append("text")
+        .attr("dx", 10)
+        .attr("dy", 5)
+        .style("font-weight", 100)
+        .classed("node-text", true)
+        .text(function(d){
+            return d.name
+        });
+    {% else %}
+    node.append("title")
+        .text(function(d) { return d.name; });
+    {% endif %}
+
+    function tick() {
+        if(browser == "ie") {
+            // internet explorer
+            link.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+        } else {
+            // firefox, etc.
+            link.attr("d", function(d) {
+                let deltaX = d.target.x - d.source.x,
+                    deltaY = d.target.y - d.source.y,
+                    dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
+                    normX = deltaX / dist,
+                    normY = deltaY / dist,
+                    sourcePadding = node_size,
+                    targetPadding = node_size + 3,
+                    sourceX = d.source.x + (sourcePadding * normX),
+                    sourceY = d.source.y + (sourcePadding * normY),
+                    targetX = d.target.x - (targetPadding * normX),
+                    targetY = d.target.y - (targetPadding * normY);
+                return "M" + sourceX + "," + sourceY + "L" + targetX + "," + targetY;
+            });
+        }
+
+        node.attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    }
+    simulation.on("tick", tick);
+
+    {% if attributes %}
+        node.on("mouseover", function(d){
+
+            const table  = d3.selectAll(element)
+                        .append("table")
+                        .classed("table-tooltip", true);
+
+            const thead = table.append('thead')
+            const tbody = table.append('tbody');
+            
+            const columns = ["attribute", "value"]
+            thead.append('tr')
+                .selectAll('th')
+                .data(columns).enter()
+                .append('th')
+                .text(function (column) { return column; });
+    
+            const rows = tbody.selectAll('tr')
+                            .data(d["attributes"])
+                            .enter()
+                            .append('tr');
+            
+            const cells = rows.selectAll('td')
+                            .data(function (d) {
+                                return columns.map(function (column) {
+                                return {column: column, value: d[column]};
+                                });
+                            })
+                            .enter()
+                            .append('td')
+                            .text(function (d) { return d.value; });
+
+        }).on("mouseout", function(){
+            d3.select(".table-tooltip").remove()
+        });
+    {% endif %}
+})
