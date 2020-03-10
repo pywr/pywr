7b958248a79cda78d82c98258f35d6f902f3aec7
James Tomlinson <tomo.bbe@gmail.com>
2020-03-10 15:53:04 +0000

    Add `ControlCurvePiecewiseInterpolatedParameter` (#857)
    * Add `ControlCurvePiecewiseInterpolatedParameter`
    
    A new parameter the interpolates between multiple control curves in
    a piecewise fashion. A pair of values is given for each "band"
    between the storage bounds and the control curves. This pair is
    used to set linear interpolation based on the current storage.
    
    * Fix docstring typo in ControlCurvePiecewiseInterpolatedParameter
    and check the dimensions of `values` inside the property setter
    rather than `__init__` function.
    
    * Add DeprecationWarning to PiecewiseLinearControlCurve.

--- pywr/parameters/control_curves.py
+++ pywr/parameters/control_curves.py
@@ -2,4 +2,4 @@
 This module contains a set of pywr._core.Parameter subclasses for defining control curve based parameters.
 """
 from ._control_curves import (PiecewiseLinearControlCurve, ControlCurveParameter, BaseControlCurveParameter,
-    ControlCurveInterpolatedParameter, ControlCurveIndexParameter)
+    ControlCurveInterpolatedParameter, ControlCurveIndexParameter, ControlCurvePiecewiseInterpolatedParameter)
