7e84d4d7b7dba8333614996b74d847d249eb50d2
James Tomlinson <tomo.bbe@gmail.com>
2020-04-04 17:20:22 +0100

    Add docstring to Aggregator.

--- pywr/recorders/_recorders.pyx
+++ pywr/recorders/_recorders.pyx
@@ -46,7 +46,39 @@ _obj_direction_lookup = {
 }
 
 cdef class Aggregator:
-    """Utility class for computing aggregate values."""
+    """Utility class for computing aggregate values.
+
+    Users are unlikely to use this class directly. Instead `Recorder` sub-classes will use this functionality
+    to aggregate their results across different dimensions (e.g. time, scenarios, etc.).
+
+    Parameters
+    ==========
+    func : str, dict or callable
+        The aggregation function to use. Can be a string or dict defining aggregation functions, or a callable
+        custom function that performs aggregation.
+
+        When a string it can be one of: "sum", "min", "max", "mean", "median", "product", or "count_nonzero". These
+        strings map to and cause the aggregator to use the corresponding `numpy` functions.
+
+        A dict can be provided containing a "func" key, and optional "args" and "kwargs" keys. The value of "func"
+        should be a string corresponding to the aforementioned numpy function names with the additional options of
+        "percentile" and "percentileofscore". These latter two functions require additional arguments (the percentile
+        and score) to function and must be provided as the values in either the "args" or "kwargs" keys of the
+        dictionary. Please refer to the corresponding numpy (or scipy) function definitions for documentation on these
+        arguments.
+
+        Finally, a callable function can be given. This function must accept either a 1D or 2D numpy array as the
+        first argument, and support the "axis" keyword as integer value that determines which axis over which the
+        function should apply aggregation. The axis keyword is only supplied when a 2D array is given. Therefore,`
+        the callable function should behave in a similar fashion to the numpy functions.
+
+    Examples
+    ========
+    >>> Aggregator("sum")
+    >>> Aggregator({"func": "percentile", "args": [95],"kwargs": {}})
+    >>> Aggregator({"func": "percentileofscore", "kwargs": {"score": 0.5, "kind": "rank"}})
+
+    """
     def __init__(self, func):
         self.func = func
 
