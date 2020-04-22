65d8d1321b1c414757ea3c022e097bde899fb7d2
James Batchelor <james.batchelor@atkinsglobal.com>
2020-04-22 12:39:11 +0100

    moved python funcs for creating d3 graph data back out of schematic class

--- pywr/notebook/__init__.py
+++ pywr/notebook/__init__.py
@@ -69,196 +69,6 @@ class PywrSchematic:
         else:
             self.css = css
 
-    def pywr_model_to_d3_json(self, model, attributes=False):
-        """
-        Convert a Pywr graph to a structure d3 can display
-
-        Parameters
-        ----------
-        model : `pywr.core.Model`
-        attributes: bool (default=False)
-            If True, attribute data for each node is extract
-        """
-        nodes = []
-        node_names = []
-        for node in model.graph.nodes():
-            if node.parent is None and node.virtual is False:
-                nodes.append(node)
-                node_names.append(node.name)
-
-        edges = []
-        for edge in model.graph.edges():
-            node_source, node_target = edge
-
-            # where a link is to/from a subnode, display a link to the parent instead
-            if node_source.parent is not None:
-                node_source = node_source.parent
-            if node_target.parent is not None:
-                node_target = node_target.parent
-
-            if node_source is node_target:
-                # link is between two subnodes
-                continue
-
-            index_source = node_names.index(node_source.name)
-            index_target = node_names.index(node_target.name)
-            edges.append({'source': index_source, 'target': index_target})
-
-        json_nodes = []
-        for n, node in enumerate(nodes):
-            node_dict = {"name": node.name}
-            classes = []
-            cls = node.__class__
-            classes.append(cls)
-            while True:
-                for base in cls.__bases__:
-                    if issubclass(base, Node) and base is not Node:
-                        classes.append(base)
-                if classes[-1] is cls:
-                    break
-                else:
-                    cls = classes[-1]
-            classes = classes[::-1]
-            node_dict["clss"] = [cls.__name__.lower() for cls in classes]
-            try:
-                node_dict["position"] = node.position["schematic"]
-            except KeyError:
-                pass
-
-            if attributes:
-                node_dict["attributes"] = self.get_node_attr(node)
-
-            json_nodes.append(node_dict)
-
-        graph = {
-            "nodes": json_nodes,
-            "links": edges}
-
-        return graph
-
-    def get_node_attr(self, node):
-        """
-        Returns a dictionary that contains node attributes as strings
-
-        Parameters
-        ----------
-        node : a pywr node object
-        """
-        attrs = inspect.getmembers(node, lambda a:not(inspect.isroutine(a)))
-        attribute_data = []
-        for att in attrs:
-
-            attr_name, attr_val = att
-            if attr_name.startswith("_"):
-                continue
-            attr_type = type(attr_val).__name__
-
-            attrs_to_skip = ["component_attrs", "components", "color", "model",  "input", "output",
-                            "inputs", "outputs", "sub_domain", "sub_output", "sublinks", "visible",
-                            "fully_qualified_name", "allow_isolated"]
-            if not attr_val or attr_name.lower() in attrs_to_skip:
-                continue
-
-            if isinstance(attr_val, Component):
-                attr_val = attr_val.name
-                if not attr_val:
-                    attr_val = attr_type
-                else:
-                    attr_val = attr_val + " - " + attr_type
-
-            if isinstance(attr_val, list):
-                new_vals = []
-                for val in attr_val:
-                    val_name = str(val)
-                    val_name = val_name.replace("[", "").replace("]", "")
-                    new_vals.append(val_name)
-                attr_val = "".join(new_vals)
-            else:
-                attr_val = str(attr_val)
-
-            attribute_data.append({"attribute": attr_name, "value": attr_val})
-
-        return attribute_data
-
-    def pywr_json_to_d3_json(self, model, attributes=False):
-        """
-        Converts a JSON file or a JSON-derived dict into structure that d3js can use.
-
-        Parameters
-        ----------
-        model : dict or str
-            str inputs should be a path to a json file containing the model.
-        """
-
-        if isinstance(model, str):
-            with open(model) as d:
-                model = json.load(d)
-
-        nodes = [node["name"] for node in model["nodes"]]
-
-        edges = []
-        for edge in model["edges"]:
-            sourceindex = nodes.index(edge[0])
-            targetindex = nodes.index(edge[1])
-            edges.append({'source': sourceindex, 'target': targetindex})
-
-        nodes = []
-        node_classes = self.create_node_class_trees()
-
-        for node in model["nodes"]:
-            json_node = {'name': node["name"], 'clss': node_classes[node["type"].lower()]}
-            try:
-                json_node['position'] = node['position']['schematic']
-            except KeyError:
-                pass
-
-            if attributes:
-                json_node["attributes"] = []
-                for name, val in node.items():
-
-                    if name == "type":
-                        continue
-
-                    attr_val = val
-
-                    if isinstance(val, dict):
-                        try:
-                            attr_val = get_parameter_from_registry(val["type"]).__name__
-                        except KeyError:
-                            pass
-                    elif val in model["parameters"].keys():
-                        param = model["parameters"][val]
-                        attr_type = get_parameter_from_registry(param["type"]).__name__
-                        attr_val = attr_val + " - " + attr_type
-
-                    attr_dict = {"attribute": name, "value": attr_val}
-                    json_node["attributes"].append(attr_dict)
-
-            nodes.append(json_node)
-
-        graph = {
-            "nodes": nodes,
-            "links": edges}
-
-        return graph
-
-    def create_node_class_trees(self):
-        # create class tree for each node type
-        node_class_trees = {}
-        for name, cls in NodeMeta.node_registry.items():
-            classes = [cls]
-            while True:
-                for base in cls.__bases__:
-                    if issubclass(base, Node) and base is not Node:
-                        classes.append(base)
-                if classes[-1] is cls:
-                    break
-                else:
-                    cls = classes[-1]
-            clss = [cls.__name__.lower() for cls in classes[::-1]]
-            node_class_trees[name] = clss
-        return node_class_trees
-
     def draw_graph(self):
         """Draw pywr schematic graph in a jupyter notebook"""
         js = draw_graph_template.render(
@@ -309,3 +119,197 @@ class PywrSchematic:
 
         with open(filename, "w") as f:
             f.write(html)
+
+
+def pywr_model_to_d3_json(model, attributes=False):
+    """
+    Convert a Pywr graph to a structure d3 can display
+
+    Parameters
+    ----------
+    model : `pywr.core.Model`
+    attributes: bool (default=False)
+        If True, attribute data for each node is extract
+    """
+    nodes = []
+    node_names = []
+    for node in model.graph.nodes():
+        if node.parent is None and node.virtual is False:
+            nodes.append(node)
+            node_names.append(node.name)
+
+    edges = []
+    for edge in model.graph.edges():
+        node_source, node_target = edge
+
+        # where a link is to/from a subnode, display a link to the parent instead
+        if node_source.parent is not None:
+            node_source = node_source.parent
+        if node_target.parent is not None:
+            node_target = node_target.parent
+
+        if node_source is node_target:
+            # link is between two subnodes
+            continue
+
+        index_source = node_names.index(node_source.name)
+        index_target = node_names.index(node_target.name)
+        edges.append({'source': index_source, 'target': index_target})
+
+    json_nodes = []
+    for n, node in enumerate(nodes):
+        node_dict = {"name": node.name}
+        classes = []
+        cls = node.__class__
+        classes.append(cls)
+        while True:
+            for base in cls.__bases__:
+                if issubclass(base, Node) and base is not Node:
+                    classes.append(base)
+            if classes[-1] is cls:
+                break
+            else:
+                cls = classes[-1]
+        classes = classes[::-1]
+        node_dict["clss"] = [cls.__name__.lower() for cls in classes]
+        try:
+            node_dict["position"] = node.position["schematic"]
+        except KeyError:
+            pass
+
+        if attributes:
+            node_dict["attributes"] = get_node_attr(node)
+
+        json_nodes.append(node_dict)
+
+    graph = {
+        "nodes": json_nodes,
+        "links": edges}
+
+    return graph
+
+
+def get_node_attr(node):
+    """
+    Returns a dictionary that contains node attributes as strings
+
+    Parameters
+    ----------
+    node : a pywr node object
+    """
+    attrs = inspect.getmembers(node, lambda a:not(inspect.isroutine(a)))
+    attribute_data = []
+    for att in attrs:
+
+        attr_name, attr_val = att
+        if attr_name.startswith("_"):
+            continue
+        attr_type = type(attr_val).__name__
+
+        attrs_to_skip = ["component_attrs", "components", "color", "model",  "input", "output",
+                        "inputs", "outputs", "sub_domain", "sub_output", "sublinks", "visible",
+                        "fully_qualified_name", "allow_isolated"]
+        if not attr_val or attr_name.lower() in attrs_to_skip:
+            continue
+
+        if isinstance(attr_val, Component):
+            attr_val = attr_val.name
+            if not attr_val:
+                attr_val = attr_type
+            else:
+                attr_val = attr_val + " - " + attr_type
+
+        if isinstance(attr_val, list):
+            new_vals = []
+            for val in attr_val:
+                val_name = str(val)
+                val_name = val_name.replace("[", "").replace("]", "")
+                new_vals.append(val_name)
+            attr_val = "".join(new_vals)
+        else:
+            attr_val = str(attr_val)
+
+        attribute_data.append({"attribute": attr_name, "value": attr_val})
+
+    return attribute_data
+
+
+def pywr_json_to_d3_json(model, attributes=False):
+    """
+    Converts a JSON file or a JSON-derived dict into structure that d3js can use.
+
+    Parameters
+    ----------
+    model : dict or str
+        str inputs should be a path to a json file containing the model.
+    """
+
+    if isinstance(model, str):
+        with open(model) as d:
+            model = json.load(d)
+
+    nodes = [node["name"] for node in model["nodes"]]
+
+    edges = []
+    for edge in model["edges"]:
+        sourceindex = nodes.index(edge[0])
+        targetindex = nodes.index(edge[1])
+        edges.append({'source': sourceindex, 'target': targetindex})
+
+    nodes = []
+    node_classes = create_node_class_trees()
+
+    for node in model["nodes"]:
+        json_node = {'name': node["name"], 'clss': node_classes[node["type"].lower()]}
+        try:
+            json_node['position'] = node['position']['schematic']
+        except KeyError:
+            pass
+
+        if attributes:
+            json_node["attributes"] = []
+            for name, val in node.items():
+
+                if name == "type":
+                    continue
+
+                attr_val = val
+
+                if isinstance(val, dict):
+                    try:
+                        attr_val = get_parameter_from_registry(val["type"]).__name__
+                    except KeyError:
+                        pass
+                elif val in model["parameters"].keys():
+                    param = model["parameters"][val]
+                    attr_type = get_parameter_from_registry(param["type"]).__name__
+                    attr_val = attr_val + " - " + attr_type
+
+                attr_dict = {"attribute": name, "value": attr_val}
+                json_node["attributes"].append(attr_dict)
+
+        nodes.append(json_node)
+
+    graph = {
+        "nodes": nodes,
+        "links": edges}
+
+    return graph
+
+
+def create_node_class_trees():
+    # create class tree for each node type
+    node_class_trees = {}
+    for name, cls in NodeMeta.node_registry.items():
+        classes = [cls]
+        while True:
+            for base in cls.__bases__:
+                if issubclass(base, Node) and base is not Node:
+                    classes.append(base)
+            if classes[-1] is cls:
+                break
+            else:
+                cls = classes[-1]
+        clss = [cls.__name__.lower() for cls in classes[::-1]]
+        node_class_trees[name] = clss
+    return node_class_trees
