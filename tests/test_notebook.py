f965ddd01a40f69032576c182528e7067a14bb28
James Batchelor <james.batchelor@atkinsglobal.com>
2020-05-11 00:09:39 +0100

    added docstring to graph data test

--- tests/test_notebook.py
+++ tests/test_notebook.py
@@ -46,7 +46,9 @@ def test_from_json(from_json):
 
 
 def test_d3_data():
-
+    """ Check that the graph data returned by the pywr_json_to_d3_json and pywr_model_to_d3_json functions contains the
+    same nodes and number on links. The data won't match exactly due to differences in node ordering.
+    """
     json_path = os.path.join(os.path.dirname(__file__), "models", "demand_saving2_with_variables.json")
     model = load_model("demand_saving2_with_variables.json")
 
