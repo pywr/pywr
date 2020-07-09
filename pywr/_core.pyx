b36036796d2aac4b7581017e3f82c0050d14d687
James Tomlinson <tomo.bbe@gmail.com>
2020-07-09 18:28:54 +0100

    A better workaround for #470 (#690)
    * A better workaround for #470
    
    This change the reservoir volume initialisation in the case where
    max_volume is parameter. Now both `initial_volume` and
    `initial_volume_pc` must be specified as floats, and are used as
    is. This removes the current whitelist of acceptable parameters.
    
    It does however mean that if the user specifies initial conditions
    that are not compatible with the actual max_volume (i.e.
    initial_volume != initial_volume_pc/max_volume) then there is the
    possibility that parameter values will be incorrect on the first
    time-step. This changes effectively makes that the users
    responsibility.
    
    * Add parameters section docstring of Storage class.

--- pywr/_core.pyx
+++ pywr/_core.pyx
@@ -823,7 +823,7 @@ cdef class AbstractStorage(AbstractNode):
 
 cdef class Storage(AbstractStorage):
     def __cinit__(self, ):
-        self.initial_volume = 0.0
+        self.initial_volume = None
         self.initial_volume_pc = None
         self._min_volume = 0.0
         self._max_volume = 0.0
@@ -987,48 +987,54 @@ cdef class Storage(AbstractStorage):
         """
         cdef int i
         cdef double mxv = self._max_volume
-        cdef ScenarioIndex si
-        cdef Parameter p
-        # These are the supported aggregated style parameters that can be used for max_volume
-        # They only work if all their children have no children themselves.
-        from .parameters._parameters import AggregatedParameter, AggregatedIndexParameter, IndexedArrayParameter
-
-        # TODO at some point remove this limitation
-        # See issue #470 https://github.com/pywr/pywr/issues/470
-        if self._max_volume_param is not None:
-            if isinstance(self._max_volume_param,
-                          (AggregatedParameter, AggregatedIndexParameter, IndexedArrayParameter)):
-                # Some simple aggregated style parameters are accepted so long as they have simple children
-                for p in self._max_volume_param.children:
-                    if len(p.children) > 0:
-                        raise RuntimeError(
-                            'Only children of agregated parameters with no dependencies are supported for max_volume.')
-                    p.calc_values(self._model.timestepper.current)
-
-            elif len(self._max_volume_param.children) > 0:
-                raise RuntimeError('Only parameters with no dependencies are supported for max_volume.')
-            # We ensure that this is called in reset
-            self._max_volume_param.calc_values(self._model.timestepper.current)
+        cdef double reset_volume, reset_pc
 
         for i, si in enumerate(self.model.scenarios.combinations):
-            # Ensure variable maximum volume is taken in to account
+
             if self._max_volume_param is not None:
-                mxv = self._max_volume_param.get_value(si)
+                # Ensure variable maximum volume is taken in to account
+                if use_initial_volume:
+                    # Max volume is a parameter; require both initial_volume and initial_volume_pc be given.
+                    # The parameter will not be evaluated at the beginning of the model run.
+                    if not np.isfinite(self._initial_volume_pc) or not np.isfinite(self._initial_volume):
+                        raise RuntimeError('Both `initial_volume` and `initial_volume_pc` must be supplied if'
+                                           '`max_volume` is defined as a parameter.')
 
-            if use_initial_volume:
-                if np.isfinite(self._initial_volume_pc):
-                    self._volume[i] = self._initial_volume_pc * mxv
-                elif np.isfinite(self._initial_volume):
-                    self._volume[i] = self._initial_volume
-                else:
-                    raise RuntimeError('Initial volume must be set as either a percentage or absolute volume.')
+                    reset_volume = self._initial_volume
+                    reset_pc = self._initial_volume_pc
+                elif self.model.timestep.index > 0:
+                    # If it's not the first time-step, and we want to reset to max capacity then the
+                    # parameter should already have been evaluated.
+                    mxv = self._max_volume_param.get_value(si)
+                    # Reset to max capacity, accounting for variable volume
+                    reset_volume = mxv
+                    # We do this division (rather than setting reset_pc = 1.0) to be consistent with the case below
+                    # when mxv is zero.
+                    try:
+                        reset_pc = reset_volume / mxv
+                    except ZeroDivisionError:
+                        reset_pc = np.nan
             else:
-                self._volume[i] = mxv
+                # Max volume is a double.
+                if use_initial_volume:
+                    # User only has to supply absolute or relative initial volume
+                    if np.isfinite(self._initial_volume_pc):
+                        reset_volume = self._initial_volume_pc * mxv
+                    elif np.isfinite(self._initial_volume):
+                        reset_volume = self._initial_volume
+                    else:
+                        raise RuntimeError('Initial volume must be set as either a percentage or absolute volume.')
+                else:
+                    reset_volume = mxv
 
-            try:
-                self._current_pc[i] = self._volume[i] / mxv
-            except ZeroDivisionError:
-                self._current_pc[i] = np.nan
+                # Compute the proportional volume.
+                try:
+                    reset_pc = reset_volume / mxv
+                except ZeroDivisionError:
+                    reset_pc = np.nan
+
+            self._volume[i] = reset_volume
+            self._current_pc[i] = reset_pc
 
     cpdef after(self, Timestep ts):
         AbstractStorage.after(self, ts)
--- pywr/nodes.py
+++ pywr/nodes.py
@@ -266,6 +266,30 @@ class Storage(Drawable, Connectable, _core.Storage, metaclass=NodeMeta):
     If a recorder is set on the storage node, instead of recording flow it
     records changes in storage. Any recorders set on the output or input
     sub-nodes record flow as normal.
+
+    Parameters
+    ----------
+    model : Model
+        Model instance to which this storage node is attached.
+    name : str
+        The name of the storage node.
+    num_inputs, num_outputs : integer (optional)
+        The number of input and output nodes to create internally. Defaults to 1.
+    min_volume : float (optional)
+        The minimum volume of the storage. Defaults to 0.0.
+    max_volume : float, Parameter (optional)
+        The maximum volume of the storage. Defaults to 0.0.
+    initial_volume, initial_volume_pc : float (optional)
+        Specify initial volume in either absolute or proportional terms. Both are required if `max_volume`
+        is a parameter because the parameter will not be evaluated at the first time-step. If both are given
+        and `max_volume` is not a Parameter, then the absolute value is ignored.
+    cost : float, Parameter (optional)
+        The cost of net flow in to the storage node. I.e. a positive cost penalises increasing volume by
+        giving a benefit to negative net flow (release), and a negative cost penalises decreasing volume
+        by giving a benefit to positive net flow (inflow).
+    area, level : float, Parameter (optional)
+        Optional float or Parameter defining the area and level of the storage node. These values are
+        accessible through the `get_area` and `get_level` methods respectively.
     """
     def __init__(self, model, name, num_outputs=1, num_inputs=1, *args, **kwargs):
         # cast number of inputs/outputs to integer
@@ -277,7 +301,7 @@ class Storage(Drawable, Connectable, _core.Storage, metaclass=NodeMeta):
         if min_volume is None:
             min_volume = 0.0
         max_volume = pop_kwarg_parameter(kwargs, 'max_volume', 0.0)
-        initial_volume = kwargs.pop('initial_volume', 0.0)
+        initial_volume = kwargs.pop('initial_volume', None)
         initial_volume_pc = kwargs.pop('initial_volume_pc', None)
         cost = pop_kwarg_parameter(kwargs, 'cost', 0.0)
         level = pop_kwarg_parameter(kwargs, 'level', None)
@@ -356,11 +380,7 @@ class Storage(Drawable, Connectable, _core.Storage, metaclass=NodeMeta):
         name = data.pop('name')
         num_inputs = int(data.pop('inputs', 1))
         num_outputs = int(data.pop('outputs', 1))
-
-        if 'initial_volume' not in data and 'initial_volume_pc' not in data:
-            raise ValueError('Initial volume must be specified in absolute or relative terms.')
-
-        initial_volume = data.pop('initial_volume', 0.0)
+        initial_volume = data.pop('initial_volume', None)
         initial_volume_pc = data.pop('initial_volume_pc', None)
         max_volume = data.pop('max_volume')
         min_volume = data.pop('min_volume', 0.0)
@@ -376,10 +396,11 @@ class Storage(Drawable, Connectable, _core.Storage, metaclass=NodeMeta):
         # loading errors
 
         # Try to coerce initial volume to float.
-        try:
-            initial_volume = float(initial_volume)
-        except TypeError:
-            initial_volume = load_parameter_values(model, initial_volume)
+        if initial_volume is not None:
+            try:
+                initial_volume = float(initial_volume)
+            except TypeError:
+                initial_volume = load_parameter_values(model, initial_volume)
         node.initial_volume = initial_volume
         node.initial_volume_pc = initial_volume_pc
 
