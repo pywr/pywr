c3c1e380042a4a0fc3aab355adefc2477ce56bcb
James Tomlinson <tomo.bbe@gmail.com>
2020-09-16 09:45:27 +0100

    Import ScenarioIndex in pywr.core

--- pywr/core.py
+++ pywr/core.py
@@ -2,5 +2,5 @@ from pywr.timestepper import Timestepper  # noqa
 from pywr.model import Model  # noqa
 from pywr.model import ModelStructureError, ModelDocumentWarning  # noqa TODO: exceptions module?
 from pywr.nodes import *  # noqa
-from pywr._core import Scenario, ScenarioCollection, Timestep  # noqa
+from pywr._core import Scenario, ScenarioCollection, ScenarioIndex, Timestep  # noqa
 from pywr._component import Component  # noqa
