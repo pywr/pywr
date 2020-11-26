74d00ab75fa825846518f0fb34777e81d3164dfd
James Tomlinson <tomo.bbe@gmail.com>
2020-11-26 13:12:55 +0000

    Add Windows specific tests script and +x permission on Linux test script.

---
+++ .github/workflows/run-tests-windows.bat
@@ -0,0 +1,10 @@
+
+echo "Running tests in: %1";
+SET PYWR_SOLVER=glpk
+python -m pytest "%1"\tests;
+
+SET PYWR_SOLVER=glpk-edge
+python -m pytest "%1"\tests;
+
+SET PYWR_SOLVER=lpsolve
+python -m pytest "%1"\tests;
