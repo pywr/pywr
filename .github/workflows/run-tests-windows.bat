419ec5a7574a9d20cd56ab574772cf7a35e8aae8
James Tomlinson <tomo.bbe@gmail.com>
2020-11-26 14:43:07 +0000

    Fix Windows pytest path.

--- .github/workflows/run-tests-windows.bat
+++ .github/workflows/run-tests-windows.bat
@@ -1,13 +1,11 @@
 
 echo "Running tests in: %1";
 
-dir %1;
-
 SET PYWR_SOLVER=glpk
-python -m pytest "%1"\tests;
+python -m pytest "%1"\tests
 
 SET PYWR_SOLVER=glpk-edge
-python -m pytest "%1"\tests;
+python -m pytest "%1"\tests
 
 SET PYWR_SOLVER=lpsolve
-python -m pytest "%1"\tests;
+python -m pytest "%1"\tests
