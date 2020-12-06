ca849c71a036e311ff62979c77301c41d69159e7
snorfalorpagus <snorfalorpagus@users.noreply.github.com>
2020-12-06 22:39:23 +0000

    Deploying to gh-pages from  @ 6d63ada6c303a7d05bdd8abdb5675e40d6ad4864 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.9.1.dev14+g712f8de',
+    VERSION: '1.9.1.dev15+g6d63ada',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
