158d82e4f1142a2c20b4360fc1433555e5f8c8c4
James Tomlinson <tomo.bbe@gmail.com>
2022-03-16 14:17:31 +0000

    Add OtherModelIndexParameterValueIndexParameter

--- pywr/parameters/multi_model_parameters.py
+++ pywr/parameters/multi_model_parameters.py
@@ -1,4 +1,4 @@
-from ._parameters import Parameter
+from ._parameters import Parameter, IndexParameter
 
 
 class OtherModelParameterValueParameter(Parameter):
@@ -27,6 +27,34 @@ class OtherModelParameterValueParameter(Parameter):
 
 
 OtherModelParameterValueParameter.register()
+
+
+class OtherModelIndexParameterValueIndexParameter(IndexParameter):
+    """Parameter that obtains its value from another model's parameter."""
+
+    def __init__(self, model, other_model, parameter, **kwargs):
+        super().__init__(model, **kwargs)
+        self.other_model = other_model
+        self.parameter = parameter
+
+        self._other_model = None
+        self._other_model_parameter = None
+
+    def setup(self):
+        super().setup()
+        # Find the references to the other model and one of its parameters.
+        self._other_model = self.model.parent.models[self.other_model]
+        self._other_model_parameter = self._other_model.parameters[self.parameter]
+
+    def index(self, ts, scenario_index):
+        return self._other_model_parameter.get_index(scenario_index)
+
+    @classmethod
+    def load(cls, model, data):
+        return cls(model, **data)
+
+
+OtherModelIndexParameterValueIndexParameter.register()
 
 
 class OtherModelNodeFlowParameter(Parameter):
