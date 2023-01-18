a13cc858d3ce57a9b0e19bcd8fd5e21c8486eff8
James Batchelor <jamesbatchelor@outlook.com>
2023-01-18 11:32:05 +0000

    expand table rec docstring to included information on time and scenario tables

--- pywr/recorders/recorders.py
+++ pywr/recorders/recorders.py
@@ -232,11 +232,21 @@ CSVRecorder.register()
 
 class TablesRecorder(Recorder):
     """
-    A recorder that saves to PyTables CArray
+    A recorder that saves model outputs to an HDF file.
 
-    This Recorder creates a CArray for every node passed to the constructor.
-    Each CArray stores the data for all scenarios on the specific node. This
-    is useful for analysis of Node statistics across multiple scenarios.
+    This Recorder creates a CArray in the HDF file for every node passed to the
+    constructor. Each CArray stores the data for all scenarios on the specific node.
+    This is useful for analysis of Node statistics across multiple scenarios.
+    Parameter values can also be optionally stored in CArrays within the file.
+
+    By default, the recorder also stores time and scenario metadata tables. The
+    time table stores a row containing index, year, month and day values for every
+    timestep. A scenario table is created containing the name and size of each
+    scenario defined for the Model. If scenario combinations are defined for the
+    model then a separate table is created that saves the scenario indices of each
+    combination. If there are no combinations but some of the scenarios have slices
+    defined then scenario slice information, including the slice start, end, and
+    step, is stored in a table.
     """
 
     def __init__(
