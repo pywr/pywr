b2bd69dd683f5b0238d800df593b948976d2fd6d
jetuk <tomo.bbe@gmail.com>
2023-02-23 12:23:03 +0000

    Deploying to gh-pages from  @ 48e2462b1836ba09198fead1991ea501b9ed434c 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.20.1.dev2+g7cbf53ca',
+    VERSION: '1.20.1.dev3+g48e2462b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
