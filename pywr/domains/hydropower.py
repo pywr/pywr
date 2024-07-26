2fcb43e6ce439f7a80b5f48196d5e2c88552a38f
Stephen Knox <sknox@nexsys-analytics.com>
2024-07-26 16:34:44 +0100

    Move the addition of the recorders in the turbine node to the finalise_load function so that they have access to the storage node's level parameter

--- pywr/domains/hydropower.py
+++ pywr/domains/hydropower.py
@@ -53,48 +53,24 @@ class Turbine(Link, metaclass=NodeMeta):
     """
     def __init__(self, model, name, **kwargs):
         hp_recorder_kwarg_names = ('efficiency', 'density', 'flow_unit_conversion', 'energy_unit_conversion')
-        hp_kwargs = {}
+        self.hp_kwargs = {}
         for kwd in hp_recorder_kwarg_names:
             try:
-                hp_kwargs[kwd] = kwargs.pop(kwd)
+                self.hp_kwargs[kwd] = kwargs.pop(kwd)
             except KeyError:
                 pass
 
-        level_parameter = None
-        storage_node = kwargs.pop("storage_node", None)
-
-        if storage_node is not None:
-            storage_node = model.pre_load_node(storage_node)
-            self.storage_node = storage_node
-            if hasattr(storage_node, "level") and storage_node.level is not None:
-                if not isinstance(storage_node.level, Parameter):
-                    level_parameter = ConstantParameter(model, value=storage_node.level)
-                else:
-                    level_parameter = storage_node.level
-
-        turbine_elevation = kwargs.pop('turbine_elevation', 0)
-        generation_capacity = kwargs.pop('generation_capacity', 0)
-        min_operating_elevation = kwargs.pop('min_operating_elevation', 0)
-        min_head = min_operating_elevation - turbine_elevation
+        self.storage_node = kwargs.pop("storage_node", None)
+        self.turbine_elevation = kwargs.pop('turbine_elevation', 0)
+        self.generation_capacity = kwargs.pop('generation_capacity', 0)
+        self.min_operating_elevation = kwargs.pop('min_operating_elevation', 0)
+        self.min_head = self.min_operating_elevation - self.turbine_elevation
 
         super().__init__(model, name, **kwargs)
 
-        if isinstance(generation_capacity, (float, int)):
-            generation_capacity = ConstantParameter(model, generation_capacity)
+        if isinstance(self.generation_capacity, (float, int)):
+            self.generation_capacity = ConstantParameter(model, self.generation_capacity)
 
-        hp_target_flow = HydropowerTargetParameter(model, generation_capacity,
-                                                   water_elevation_parameter=level_parameter,
-                                                   min_head=min_head, min_flow=ConstantParameter(model, 0),
-                                                   turbine_elevation=turbine_elevation,
-                                                   **hp_kwargs)
-
-        self.max_flow = hp_target_flow
-
-        hp_recorder = HydropowerRecorder(model, self,
-                                         name=f"__{name}__:hydropowerrecorder",
-                                         water_elevation_parameter=level_parameter,
-                                         turbine_elevation=turbine_elevation, **hp_kwargs)
-        self.hydropower_recorder = hp_recorder
 
     @classmethod
     def pre_load(cls, model, data):
@@ -116,3 +92,35 @@ class Turbine(Link, metaclass=NodeMeta):
         setattr(node, "_Loadable__parameters_to_load", {})
 
         return node
+    
+    def finalise_load(self):
+        super().finalise_load()
+
+        level_parameter = None
+        if self.storage_node is not None:
+            storage_node = self.model.nodes[self.storage_node]
+            if hasattr(storage_node, 'level'):
+                if storage_node._Loadable__parameters_to_load.get('level'):
+                    level_parameter = self.model.parameters[storage_node._Loadable__parameters_to_load['level']]
+                else:
+                    try:
+                        float(storage_node.level)
+                        level_parameter = ConstantParameter(self.model, storage_node.level)
+                    except:
+                        raise Exception(f"An unknown value {storage_node.level} was set on the storage node level.")
+
+        hp_target_flow = HydropowerTargetParameter(self.model, self.generation_capacity,
+                                                   water_elevation_parameter=level_parameter,
+                                                   min_head=self.min_head, min_flow=self.min_flow,
+                                                   turbine_elevation=self.turbine_elevation,
+                                                   **self.hp_kwargs)
+
+        self.max_flow = hp_target_flow
+
+        hp_recorder = HydropowerRecorder(self.model, self,
+                                         name=f"__{self.name}__:hydropowerrecorder",
+                                         water_elevation_parameter=level_parameter,
+                                         turbine_elevation=self.turbine_elevation, **self.hp_kwargs)
+        self.hydropower_recorder = hp_recorder
+
+        print("hello")
