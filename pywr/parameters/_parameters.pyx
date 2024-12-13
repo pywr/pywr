fda1086ba71585ae4852b892da625567ce366dbe
James Batchelor <jamesbatchelor@outlook.com>
2024-12-13 23:23:19 +0000

    fix: numpy test errors

--- pywr/parameters/_parameters.pyx
+++ pywr/parameters/_parameters.pyx
@@ -1077,7 +1077,7 @@ cdef class RbfProfileParameter(Parameter):
                 raise ValueError('At least 3 days of the year are required.')
             if np.any(np.diff(values) <= 0):
                 raise ValueError('The days of the year should be strictly monotonically increasing.')
-            if np.any(0 > values > 365):
+            if np.any((0 > values) | (values > 365)):
                 raise ValueError('Days of the years should be between 1 and 365 inclusive.')
             self._days_of_year = values
 
