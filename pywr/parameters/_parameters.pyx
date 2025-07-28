2dd7dffb8208d14e7161465e7ef35ebc68f20415
Stefano Simoncelli <Stefano.Simoncelli@dwrcymru.com>
2025-07-28 14:40:11 +0100

    feat: Set timestep_offset in TablesArrayParameter when model is loaded via JSON

--- pywr/parameters/_parameters.pyx
+++ pywr/parameters/_parameters.pyx
@@ -508,7 +508,7 @@ cdef class TablesArrayParameter(IndexParameter):
         for algo, hash in checksums.items():
             model.check_hash(url, hash, algorithm=algo)
 
-        return cls(model, url, node, where=where, scenario=scenario)
+        return cls(model, url, node, where=where, scenario=scenario, timestep_offset=data.pop('timestep_offset', 0))
 TablesArrayParameter.register()
 
 
