bb4f15ea6c5a20d89506545179cef0d2944ea638
jetuk <tomo.bbe@gmail.com>
2025-09-19 08:42:27 +0000

    Deploying to gh-pages from  @ 80140ae5d703bbbcc3759d21d3279356ffddddff 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.28.2.dev1+ga805119e8',
+    VERSION: '1.28.2.dev2+g80140ae5d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
